b398dbe139c638fe5a85be0ceb0bf7c527aeac50
Glenn <glenn.sorrentino@proton.me>
2023-03-28 15:23:00 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt update && sudo apt -y dist-upgrade && sudo apt -y autoremove
 
 # Install required packages
-sudo apt-get -y install git python3 python3-venv python3-pip certbot python3-certbot-nginx nginx whiptail tor python3-smtplib
+sudo apt-get -y install git python3 python3-venv python3-pip certbot python3-certbot-nginx nginx whiptail tor
 
 # Function to display error message and exit
 error_exit() {
