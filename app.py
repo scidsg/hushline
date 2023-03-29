d8380e67ea31ff3f8c88441bf81636127b54571e
Glenn <glenn.sorrentino@proton.me>
2023-03-28 21:16:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,40 +2,25 @@ from flask import Flask, render_template, request, jsonify
 from flask_mail import Mail, Message
 import os
 import pgpy
-import base64
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
 
 app = Flask(__name__)
 
 # Configure Flask-Mail
 app.config['MAIL_SERVER'] = os.environ['MAIL_SERVER']
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
-app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD_HASHED']
+app.config['MAIL_USERNAME'] = os.environ['EMAIL']
+app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
 app.config['MAIL_USE_SSL'] = True
 
 mail = Mail(app)
-
-def decrypt_password(encrypted_password, secret_key):
-    secret_key = bytes.fromhex(secret_key)  # Add this line to convert the secret_key to bytes
-    cipher = AES.new(secret_key, AES.MODE_ECB)
-    decrypted_password = cipher.decrypt(base64.b64decode(encrypted_password))
-    return decrypted_password.decode("utf-8").strip()
-
+  
 def encrypt_message(message, public_key_path):
     with open(public_key_path, 'r') as key_file:
         key_data = key_file.read()
     public_key, _ = pgpy.PGPKey.from_blob(key_data)  # Extract the key from the tuple
     encrypted_message = str(public_key.encrypt(pgpy.PGPMessage.new(message)))
     return encrypted_message
-
 def send_email(encrypted_message):
-    decrypted_password = decrypt_password(os.environ['MAIL_PASSWORD_HASHED'], os.environ['SECRET_KEY'])
-    
-    # Update the app configuration with the decrypted password
-    app.config['MAIL_PASSWORD'] = decrypted_password
-    
     msg = Message("New Encrypted Message", sender=app.config['MAIL_USERNAME'], recipients=[os.environ['EMAIL']])
     msg.body = "You have received a new encrypted message:\n\n" + encrypted_message
     mail.send(msg)
@@ -43,8 +28,8 @@ def send_email(encrypted_message):
 @app.route('/')
 def index():
     return render_template('index.html')
-
 @app.route('/save_message', methods=['POST'])
+
 def save_message():
     message = request.form['message']
     encrypted_message = encrypt_message(message, 'public_key.asc')
@@ -52,6 +37,5 @@ def save_message():
         f.write(encrypted_message + '\n\n')
     send_email(encrypted_message)
     return jsonify({'success': True})
-
 if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
