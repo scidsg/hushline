9709f7b857c3043e048390c7e49b6230df7819f7
Glenn <glenn.sorrentino@proton.me>
2023-03-28 21:41:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request, jsonify
+from flask_mail import Mail, Message
 import os
 import pgpy
 
 app = Flask(__name__)
+
+# Configure Flask-Mail
+app.config['MAIL_SERVER'] = os.environ['MAIL_SERVER']
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.environ['EMAIL']
+app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
 
 def encrypt_message(message, public_key_path):
     with open(public_key_path, 'r') as key_file:
@@ -10,6 +20,11 @@ def encrypt_message(message, public_key_path):
     public_key, _ = pgpy.PGPKey.from_blob(key_data)  # Extract the key from the tuple
     encrypted_message = str(public_key.encrypt(pgpy.PGPMessage.new(message)))
     return encrypted_message
+
+def send_email(encrypted_message):
+    msg = Message("New Encrypted Message", sender=app.config['MAIL_USERNAME'], recipients=[os.environ['EMAIL']])
+    msg.body = "You have received a new encrypted message:\n\n" + encrypted_message
+    mail.send(msg)
 
 @app.route('/')
 def index():
