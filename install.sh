e25e29fdd233a5fff9ac122141c39ad477ba5f3c
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-03-31 13:33:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ export NOTIFY_SMTP_PORT
 echo "Domain: ${DOMAIN}"
 
 # Clone the repository
-git clone -b email-notifications https://github.com/scidsg/hush-line.git
+git clone https://github.com/scidsg/hush-line.git
 
 # Create a virtual environment and install dependencies
 cd hush-line
