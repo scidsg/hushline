4a087febb17e392f0fa987717011d0d199886c6d
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-03 20:20:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,39 @@ error_exit() {
     exit 1
 }
 
-clone_and_install_dependencies() {
+# Trap any errors and call the error_exit function
+trap error_exit ERR
+
+# Welcome Prompt
+whiptail --title "🤫 Hush Line Installation" --msgbox "Hush Line provides a simple way to receive secure messages from sources, colleagues, clients, or patients.\n\nAfter installation, you'll have a private tip line hosted on your own server, secured with PGP, HTTPS, and available on a .onion address so anyone can message you, even from locations where censorship is prevalent.\n\nBefore you begin, ensure your website's DNS settings point to this server." 16 64
+
+# Prompt user for domain name
+DOMAIN=$(whiptail --inputbox "Enter your domain name:" 8 60 3>&1 1>&2 2>&3)
+
+# Welcome Prompt
+whiptail --title "Email Setup" --msgbox "Now we'll set up email notifications. You'll receive an encrypted email when someone submits a new message.\n\nAvoid using your primary email address since your password is stored in plaintext.\n\nInstead, we recommend using a burner address or a Gmail account with a one-time password." 16 64
+
+# Prompt user for email
+EMAIL=$(whiptail --inputbox "Enter your email:" 8 60 3>&1 1>&2 2>&3)
+
+# Prompt user for email notification settings
+NOTIFY_SMTP_SERVER=$(whiptail --inputbox "Enter the SMTP server address (e.g., smtp.gmail.com):" 8 60 3>&1 1>&2 2>&3)
+NOTIFY_PASSWORD=$(whiptail --passwordbox "Enter the password for the email address:" 8 60 3>&1 1>&2 2>&3)
+NOTIFY_SMTP_PORT=$(whiptail --inputbox "Enter the SMTP server port (e.g., 465):" 8 60 3>&1 1>&2 2>&3)
+
+# Check for valid domain name format
+until [[ $DOMAIN =~ ^[a-zA-Z0-9][a-zA-Z0-9\.-]*\.[a-zA-Z]{2,}$ ]]; do
+    DOMAIN=$(whiptail --inputbox "Invalid domain name format. Please enter a valid domain name:" 8 60 3>&1 1>&2 2>&3)
+done
+export DOMAIN
+export EMAIL
+export NOTIFY_PASSWORD
+export NOTIFY_SMTP_SERVER
+export NOTIFY_SMTP_PORT
+
+# Debug: Print the value of the DOMAIN variable
+echo "Domain: ${DOMAIN}"
+
 # Clone the repository
 git clone https://github.com/scidsg/hush-line.git
 
@@ -39,14 +71,81 @@ source venv/bin/activate
 pip3 install flask
 pip3 install pgpy
 pip3 install -r requirements.txt
-}
 
-enable_hushline() {
+# Create a systemd service
+cat > /etc/systemd/system/hush-line.service << EOL
+[Unit]
+Description=Tip-Line Web App
+After=network.target
+[Service]
+User=root
+WorkingDirectory=$PWD
+Environment="DOMAIN=$DOMAIN"
+Environment="EMAIL=$EMAIL"
+Environment="NOTIFY_PASSWORD=$NOTIFY_PASSWORD"
+Environment="NOTIFY_SMTP_SERVER=$NOTIFY_SMTP_SERVER"
+Environment="NOTIFY_SMTP_PORT=$NOTIFY_SMTP_PORT"
+ExecStart=$PWD/venv/bin/python3 $PWD/app.py
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOL
+
 systemctl enable hush-line.service
 systemctl start hush-line.service
-}
 
-configure_nginx() {
+# Check if the application is running and listening on the expected address and port
+sleep 5
+if ! netstat -tuln | grep -q '127.0.0.1:5000'; then
+    echo "The application is not running as expected. Please check the application logs for more details."
+    error_exit
+fi
+
+# Create Tor configuration file
+sudo tee /etc/tor/torrc << EOL
+RunAsDaemon 1
+HiddenServiceDir /var/lib/tor/hidden_service/
+HiddenServicePort 80 127.0.0.1:5000
+EOL
+
+# Restart Tor service
+sudo systemctl restart tor.service
+sleep 10
+
+# Get the Onion address
+ONION_ADDRESS=$(sudo cat /var/lib/tor/hidden_service/hostname)
+
+# Enable the Tor hidden service
+sudo ln -sf /etc/nginx/sites-available/hush-line.nginx /etc/nginx/sites-enabled/
+sudo nginx -t && sudo systemctl restart nginx
+
+# Configure Nginx
+cat > /etc/nginx/sites-available/hush-line.nginx << EOL
+server {
+    listen 80;
+    server_name ${DOMAIN};
+    location / {
+        proxy_pass http://127.0.0.1:5000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_connect_timeout 300s;
+        proxy_send_timeout 300s;
+        proxy_read_timeout 300s;
+    }
+    
+        add_header Strict-Transport-Security "max-age=63072000; includeSubdomains";
+        add_header X-Frame-Options DENY;
+        add_header Onion-Location http://$ONION_ADDRESS\$request_uri;
+        add_header X-Content-Type-Options nosniff;
+        add_header Content-Security-Policy "default-src 'self'; frame-ancestors 'none'";
+        add_header Permissions-Policy "geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(), vibrate=(), fullscreen=(), payment=(), interest-cohort=()";
+        add_header Referrer-Policy "no-referrer";
+        add_header X-XSS-Protection "1; mode=block";
+}
+EOL
+
 # Configure Nginx
 cat > /etc/nginx/nginx.conf << EOL
 user www-data;
@@ -110,240 +209,21 @@ if [ -e "/etc/nginx/sites-enabled/default" ]; then
 fi
 ln -sf /etc/nginx/sites-available/hush-line.nginx /etc/nginx/sites-enabled/
 nginx -t && systemctl restart nginx || error_exit
-}
-
-configure_onion_service() {
-# Create Tor configuration file
-sudo tee /etc/tor/torrc << EOL
-RunAsDaemon 1
-HiddenServiceDir /var/lib/tor/hidden_service/
-HiddenServicePort 80 127.0.0.1:5000
-EOL
-
-# Restart Tor service
-sudo systemctl restart tor.service
-sleep 10
-
-# Get the Onion address
-ONION_ADDRESS=$(sudo cat /var/lib/tor/hidden_service/hostname)
-
-# Enable the Tor hidden service
-sudo ln -sf /etc/nginx/sites-available/hush-line.nginx /etc/nginx/sites-enabled/
-sudo nginx -t && sudo systemctl restart nginx
-}
-
-check_application() {
-    sleep 5
-if ! netstat -tuln | grep -q '127.0.0.1:5000'; then
-    echo "The application is not running as expected. Please check the application logs for more details."
-    error_exit
-fi
-}
-
-prompt_email() {
-# Prompt user for email
-EMAIL=$(whiptail --inputbox "Enter your email:" 8 60 3>&1 1>&2 2>&3)
-
-# Prompt user for email notification settings
-NOTIFY_SMTP_SERVER=$(whiptail --inputbox "Enter the SMTP server address (e.g., smtp.gmail.com):" 8 60 3>&1 1>&2 2>&3)
-NOTIFY_PASSWORD=$(whiptail --passwordbox "Enter the password for the email address:" 8 60 3>&1 1>&2 2>&3)
-NOTIFY_SMTP_PORT=$(whiptail --inputbox "Enter the SMTP server port (e.g., 465):" 8 60 3>&1 1>&2 2>&3)
-}
-
-# Trap any errors and call the error_exit function
-trap error_exit ERR
-
-# Prompt user to choose installation type
-INSTALL_TYPE=$(whiptail --title "Welcome to 🤫 Hush Line!" --menu "Welcome to Hush Line, your private tip line and suggestion box.\n\nYou can install Hush Line as a Tor-only implementation, or make it available on both Tor and a public website.\n\nChoose your preferred installation type:" 16 60 3 \
-    "1" "🧅 Tor-only " \
-    "2" "🧅 Tor + 🌎 Public web" 3>&1 1>&2 2>&3)
-
-if [ "$INSTALL_TYPE" != "2" ]; then
-
-# Welcome Prompt
-whiptail --title "🤫 Hush Line Installation" --msgbox "Hush Line provides a simple way to receive secure messages from sources, colleagues, clients, or patients.\n\nAfter installation, you'll have a private tip line hosted on your own server, secured with PGP, and available on a .onion address so anyone can message you, even from locations where censorship is prevalent." 16 64
-
-# Welcome Prompt
-whiptail --title "Email Setup" --msgbox "Let's set up email notifications. You'll receive an encrypted email when someone submits a new message.\n\nAvoid using your primary email address since your password is stored in plaintext.\n\nInstead, we recommend using a burner address or a Gmail account with a one-time password." 16 64
-
-# Prompt user for email notification settings
-prompt_email
-
-export EMAIL
-export NOTIFY_PASSWORD
-export NOTIFY_SMTP_SERVER
-export NOTIFY_SMTP_PORT
-
-# Clone the repository and create a virtual environment and install dependencies
-clone_and_install_dependencies
-
-# Create a systemd service
-cat > /etc/systemd/system/hush-line.service << EOL
-[Unit]
-Description=Tip-Line Web App
-After=network.target
-[Service]
-User=root
-WorkingDirectory=$PWD
-Environment="DOMAIN=localhost"
-Environment="EMAIL=$EMAIL"
-Environment="NOTIFY_PASSWORD=$NOTIFY_PASSWORD"
-Environment="NOTIFY_SMTP_SERVER=$NOTIFY_SMTP_SERVER"
-Environment="NOTIFY_SMTP_PORT=$NOTIFY_SMTP_PORT"
-ExecStart=$PWD/venv/bin/python3 $PWD/app.py
-Restart=always
-[Install]
-WantedBy=multi-user.target
-EOL
-
-enable_hushline
-
-# Check if the application is running and listening on the expected address and port
-check_application
-
-# Create Tor configuration file
-configure_onion_service
-
-# Configure Nginx
-cat > /etc/nginx/sites-available/hush-line.nginx << EOL
-server {
-    listen 80;
-    server_name localhost;
-    location / {
-        proxy_pass http://127.0.0.1:5000;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_connect_timeout 300s;
-        proxy_send_timeout 300s;
-        proxy_read_timeout 300s;
-    }
-    
-        add_header Strict-Transport-Security "max-age=63072000; includeSubdomains";
-        add_header X-Frame-Options DENY;
-        add_header Onion-Location http://$ONION_ADDRESS\$request_uri;
-        add_header X-Content-Type-Options nosniff;
-        add_header Content-Security-Policy "default-src 'self'; frame-ancestors 'none'";
-        add_header Permissions-Policy "geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(), vibrate=(), fullscreen=(), payment=(), interest-cohort=()";
-        add_header Referrer-Policy "no-referrer";
-        add_header X-XSS-Protection "1; mode=block";
-}
-EOL
-
-# Configure Nginx
-configure_nginx
-
-echo "
-✅ Installation complete!
-                                               
-http://$ONION_ADDRESS
-Hush Line is a product by Science & Design. Learn more about us at https://scidsg.org.
-Have feedback? Send us an email at hushline@scidsg.org.
-"
-fi
-
-if [ "$INSTALL_TYPE" != "1" ]; then
-
-# Welcome Prompt
-whiptail --title "🤫 Hush Line Installation" --msgbox "Hush Line provides a simple way to receive secure messages from sources, colleagues, clients, or patients.\n\nAfter installation, you'll have a private tip line hosted on your own server, secured with PGP, HTTPS, and available on a .onion address so anyone can message you, even from locations where censorship is prevalent.\n\nBefore you begin, ensure your website's DNS settings point to this server." 16 64
-
-# Prompt user for domain name
-DOMAIN=$(whiptail --inputbox "Enter your domain name:" 8 60 3>&1 1>&2 2>&3)
-
-# Welcome Prompt
-whiptail --title "Email Setup" --msgbox "Now we'll set up email notifications. You'll receive an encrypted email when someone submits a new message.\n\nAvoid using your primary email address since your password is stored in plaintext.\n\nInstead, we recommend using a burner address or a Gmail account with a one-time password." 16 64
-
-# Prompt user for email notification settings
-prompt_email
-
-# Check for valid domain name format
-until [[ $DOMAIN =~ ^[a-zA-Z0-9][a-zA-Z0-9\.-]*\.[a-zA-Z]{2,}$ ]]; do
-    DOMAIN=$(whiptail --inputbox "Invalid domain name format. Please enter a valid domain name:" 8 60 3>&1 1>&2 2>&3)
-done
-export DOMAIN
-export EMAIL
-export NOTIFY_PASSWORD
-export NOTIFY_SMTP_SERVER
-export NOTIFY_SMTP_PORT
-
-# Debug: Print the value of the DOMAIN variable
-echo "Domain: ${DOMAIN}"
-
-# Clone the repository and create a virtual environment and install dependencies
-clone_and_install_dependencies
-
-# Create a systemd service
-cat > /etc/systemd/system/hush-line.service << EOL
-[Unit]
-Description=Tip-Line Web App
-After=network.target
-[Service]
-User=root
-WorkingDirectory=$PWD
-Environment="DOMAIN=$DOMAIN"
-Environment="EMAIL=$EMAIL"
-Environment="NOTIFY_PASSWORD=$NOTIFY_PASSWORD"
-Environment="NOTIFY_SMTP_SERVER=$NOTIFY_SMTP_SERVER"
-Environment="NOTIFY_SMTP_PORT=$NOTIFY_SMTP_PORT"
-ExecStart=$PWD/venv/bin/python3 $PWD/app.py
-Restart=always
-[Install]
-WantedBy=multi-user.target
-EOL
-
-enable_hushline
-
-# Check if the application is running and listening on the expected address and port
-check_application
-
-# Create Tor configuration file
-configure_onion_service
-
-# Configure Nginx
-cat > /etc/nginx/sites-available/hush-line.nginx << EOL
-server {
-    listen 80;
-    server_name ${DOMAIN};
-    location / {
-        proxy_pass http://127.0.0.1:5000;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_connect_timeout 300s;
-        proxy_send_timeout 300s;
-        proxy_read_timeout 300s;
-    }
-    
-        add_header Strict-Transport-Security "max-age=63072000; includeSubdomains";
-        add_header X-Frame-Options DENY;
-        add_header Onion-Location http://$ONION_ADDRESS\$request_uri;
-        add_header X-Content-Type-Options nosniff;
-        add_header Content-Security-Policy "default-src 'self'; frame-ancestors 'none'";
-        add_header Permissions-Policy 'geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(), vibrate=(), fullscreen=(), payment=(), interest-cohort=()';
-        add_header Referrer-Policy "no-referrer";
-        add_header X-XSS-Protection "1; mode=block";
-}
-EOL
-
-# Configure Nginx
-configure_nginx
 
 # Obtain SSL certificate
 certbot --nginx --agree-tos --non-interactive --email ${EMAIL} --agree-tos -d $DOMAIN
 
 # Set up cron job to renew SSL certificate
 (crontab -l 2>/dev/null; echo "30 2 * * 1 /usr/bin/certbot renew --quiet") | crontab -
-
 echo "
 ✅ Installation complete!
                                                
 https://$DOMAIN
 http://$ONION_ADDRESS
+
 Hush Line is a product by Science & Design. Learn more about us at https://scidsg.org.
+
 Have feedback? Send us an email at hushline@scidsg.org.
 "
-fi
 
 # Disable the trap before exiting
