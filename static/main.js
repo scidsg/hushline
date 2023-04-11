9a418f9cb8fa6ee898de542ef10b8a60d2229f28
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-10 22:22:39 -0700

    add spinner

--- static/main.js
+++ static/main.js
@@ -1,27 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const spinner = document.querySelector(".spinner");
 
-    form.addEventListener("submit", async function(event) {
-        event.preventDefault();
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault();
 
-        const formData = new FormData(form);
-        const response = await fetch(form.action, {
-            method: 'POST',
-            body: formData
-        });
+    // Show the spinner
+    spinner.hidden = false;
 
-        // Log the server's response text
-        const responseText = await response.text();
-        console.log("Server response text:", responseText);
-
-        // Parse the response as JSON
-        const result = JSON.parse(responseText);
-
-        if (result.success) {
-            alert("Your message has been successfully encrypted and submitted.");
-            form.reset();
-        } else {
-            alert("An error occurred. Please try again.");
-        }
+    const formData = new FormData(form);
+    const response = await fetch(form.action, {
+      method: "POST",
+      body: formData,
     });
+
+    // Log the server's response text
+    const responseText = await response.text();
+    console.log("Server response text:", responseText);
+
+    // Parse the response as JSON
+    const result = JSON.parse(responseText);
+
+    if (result.success) {
+      alert("Your message has been successfully encrypted and submitted.");
+      form.reset();
+    } else {
+      alert("An error occurred. Please try again.");
+    }
+
+    // Hide the spinner
+    spinner.hidden = true;
+  });
 });
