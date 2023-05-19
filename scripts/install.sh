28bd105acb738fc470e8084dc5a8ee79bfccabb9
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-05-19 09:13:19 -0700

    add domain validation

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,7 +40,18 @@ trap error_exit ERR
 whiptail --title "🤫 Hush Line Installation" --msgbox "Hush Line provides a simple way to receive secure messages from sources, colleagues, clients, or patients.\n\nAfter installation, you'll have a private tip line hosted on your own server, secured with PGP, HTTPS, and available on a .onion address so anyone can message you, even from locations where the internet is censored.\n\nBefore you begin, ensure your website's DNS settings point to this server." 16 64
 
 # Prompt user for domain name
-DOMAIN=$(whiptail --inputbox "Enter your domain name:" 8 60 3>&1 1>&2 2>&3)
+DOMAIN=$(whiptail --inputbox "Enter your domain name (don't include http:// or https://):" 8 60 3>&1 1>&2 2>&3)
+
+# Remove 'http://' or 'https://' from the DOMAIN if it exists
+DOMAIN=${DOMAIN#http://}
+DOMAIN=${DOMAIN#https://}
+
+# Validate the domain
+until [[ $DOMAIN =~ ^[a-zA-Z0-9][a-zA-Z0-9\.-]*\.[a-zA-Z]{2,}$ ]]; do
+    DOMAIN=$(whiptail --inputbox "Invalid domain name format. Please enter a valid domain name:" 8 60 3>&1 1>&2 2>&3)
+    DOMAIN=${DOMAIN#http://}
+    DOMAIN=${DOMAIN#https://}
+done
 
 # Email notification setup
 whiptail --title "Email Setup" --msgbox "Now we'll set up email notifications. You'll receive an encrypted email when someone submits a new message.\n\nAvoid using your primary email address since your password is stored in plaintext.\n\nInstead, we recommend using a Gmail account with a one-time password." 16 64
@@ -50,10 +61,20 @@ NOTIFY_PASSWORD=$(whiptail --passwordbox "Enter the password for the email addre
 NOTIFY_SMTP_PORT=$(whiptail --inputbox "Enter the SMTP server port (e.g., 465):" 8 60 3>&1 1>&2 2>&3)
 PGP_KEY_ADDRESS=$(whiptail --inputbox "What's the address for your PGP key?" 8 60 --title "PGP Key Address" 3>&1 1>&2 2>&3)
 
-# Check for valid domain name format
-until [[ $DOMAIN =~ ^[a-zA-Z0-9][a-zA-Z0-9\.-]*\.[a-zA-Z]{2,}$ ]]; do
-    DOMAIN=$(whiptail --inputbox "Invalid domain name format. Please enter a valid domain name:" 8 60 3>&1 1>&2 2>&3)
+# Add 'https://' to the PGP_KEY_ADDRESS if it doesn't already begin with 'http://' or 'https://'
+if ! [[ $PGP_KEY_ADDRESS =~ ^http(s)?:// ]]; then
+    PGP_KEY_ADDRESS="https://$PGP_KEY_ADDRESS"
+fi
+
+# Validate the URL
+until [[ $PGP_KEY_ADDRESS =~ ^http(s)?://[a-zA-Z0-9][a-zA-Z0-9\.-]*\.[a-zA-Z]{2,}.*$ ]]; do
+    PGP_KEY_ADDRESS=$(whiptail --inputbox "Invalid URL format. Please enter a valid URL including http:// or https:// :" 8 78 --title "PGP key address" 3>&1 1>&2 2>&3)
+    if ! [[ $PGP_KEY_ADDRESS =~ ^http(s)?:// ]]; then
+        PGP_KEY_ADDRESS="https://$PGP_KEY_ADDRESS"
+    fi
 done
+
+
 export DOMAIN
 export EMAIL
 export NOTIFY_PASSWORD
