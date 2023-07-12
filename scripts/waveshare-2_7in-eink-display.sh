e1d6911cd03d8c695ca4b24909345d8d3a070f40
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-07-11 21:58:32 -0700

    Update waveshare-2_7in-eink-display.sh

--- scripts/waveshare-2_7in-eink-display.sh
+++ scripts/waveshare-2_7in-eink-display.sh
@@ -54,9 +54,11 @@ import textwrap
 import qrcode
 import requests
 import gnupg
+import traceback
 from waveshare_epd import epd2in7
 from PIL import Image, ImageDraw, ImageFont
 from PIL import ImageOps
+print(Image.__version__)
 
 def display_splash_screen(epd, image_path, display_time):
     print(f'Displaying splash screen: {image_path}')
@@ -85,14 +87,14 @@ def get_service_status():
     if status == 'active':
         return '✔ Hush Line is running'
     else:
-        return '⛌ Hush Line is not running'
+        return '⨯ Hush Line is not running'
 
 def display_status(epd, status, onion_address, name, email, key_id, expires):
     print(f'Displaying status: {status}, Onion address: {onion_address}')
     image = Image.new('1', (epd.height, epd.width), 255)
     draw = ImageDraw.Draw(image)
 
-    font_status = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 12)
+    font_status = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 13)
 
     x_pos_status = 10
     y_pos_status = 12
@@ -101,13 +103,13 @@ def display_status(epd, status, onion_address, name, email, key_id, expires):
     # Add the new text
     font_instruction = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 11)
     instruction_text = "Scan the QR code and open the link in Tor Browser to send a private message:"
-    y_pos_instruction = y_pos_status + font_status.getsize(status)[1] + 7
+    y_pos_instruction = y_pos_status + font_status.getbbox(status)[3] + 7
     max_width = epd.height - 20
-    chars_per_line = max_width // font_instruction.getsize('A')[0]
+    chars_per_line = max_width // font_instruction.getbbox('A')[2]
     wrapped_instruction = textwrap.wrap(instruction_text, width=40)
     for line in wrapped_instruction:
         draw.text((x_pos_status, y_pos_instruction), line, font=font_instruction, fill=0)
-        y_pos_instruction += font_instruction.getsize(line)[1]
+        y_pos_instruction += font_instruction.getbbox(wrapped_instruction[-1])[3] - font_instruction.getbbox(wrapped_instruction[-1])[1] + 5
 
     # Generate QR code
     qr = qrcode.QRCode(
@@ -122,7 +124,7 @@ def display_status(epd, status, onion_address, name, email, key_id, expires):
     qr_img = qr.make_image(fill_color="black", back_color="white")
 
     # Calculate available height for QR code
-    max_qr_height = epd.width - (y_pos_instruction + font_instruction.getsize(wrapped_instruction[-1])[1])
+    max_qr_height = epd.width - (y_pos_instruction + (font_instruction.getbbox(wrapped_instruction[-1])[3] - font_instruction.getbbox(wrapped_instruction[-1])[1]))
 
     width_scale_factor = max_qr_height / qr_img.width
     height_scale_factor = max_qr_height / qr_img.height
@@ -130,18 +132,27 @@ def display_status(epd, status, onion_address, name, email, key_id, expires):
     new_size = (int(qr_img.width * width_scale_factor), int(qr_img.height * height_scale_factor))
     resized_qr_img = qr_img.resize(new_size, Image.NEAREST)
 
-    x_pos = 5
-    y_pos = y_pos_instruction + font_instruction.getsize(wrapped_instruction[-1])[1] - 7
+    y_pos_instruction += font_instruction.getbbox(wrapped_instruction[-1])[3] - font_instruction.getbbox(wrapped_instruction[-1])[1] + 5
+    x_pos = x_pos_status - 3
+    y_pos = y_pos_instruction - 12
+
+    # Paste the QR code to the image
     image.paste(resized_qr_img, (x_pos, y_pos))
 
     # Calculate the starting position for the PGP information text
-    x_pos_info = x_pos + resized_qr_img.width + 5
-    y_pos_info = y_pos + 5
+    x_pos_info = x_pos + resized_qr_img.width + 10
+
+    font_info = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 11)
+
+    # Change this line to a fixed value
+    y_pos_info = 75  # initialize y_pos_info here before usage
+
+    font_info = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 11)
+    # y_pos_info = y_pos_instruction + new_size[1] + 5  # initialize y_pos_info here before usage
 
     # Display the PGP owner information
-    font_info = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 11)
     max_width = epd.height - x_pos_info - 5
-    chars_per_line = max_width // font_info.getsize('A')[0]
+    chars_per_line = max_width // (font_info.getbbox('A')[2] - font_info.getbbox('A')[0])
 
     pgp_info = f'{name} <{email}>\nKey ID: {key_id[-8:]}\nExp: {time.strftime("%Y-%m-%d", time.gmtime(int(expires)))}'
     wrapped_pgp_info = []
@@ -154,9 +165,9 @@ def display_status(epd, status, onion_address, name, email, key_id, expires):
     for i, line in enumerate(wrapped_pgp_info):
         draw.text((x_pos_info, y_pos_info), line, font=font_info, fill=0)
         if i < len(wrapped_pgp_info) - 1 and wrapped_pgp_info[i + 1] == '':
-            y_pos_info += font_info.getsize(line)[1] + empty_line_spacing
+            y_pos_info += font_info.getbbox(line)[3] + empty_line_spacing
         else:
-            y_pos_info += font_info.getsize(line)[1] + line_spacing
+            y_pos_info += font_info.getbbox(line)[3] + line_spacing
 
     # Rotate the image by 90 degrees for landscape mode
     image_rotated = image.rotate(90, expand=True)
@@ -212,9 +223,9 @@ def main():
         clear_screen(epd)
         print('Exiting...')
         sys.exit(0)
-    except Exception as e:
+    except Exception:
         clear_screen(epd)
-        print(f"Unexpected error: {e}")
+        print(f"Unexpected error:", traceback.format_exc())
         sys.exit(1)
 
 if __name__ == '__main__':
