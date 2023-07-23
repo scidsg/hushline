3673ff52b4cb6baa013a7e9efbd463d8953341e8
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-07-22 23:42:15 -0700

    Update waveshare-2_7in-eink-display.sh

--- scripts/waveshare-2_7in-eink-display.sh
+++ scripts/waveshare-2_7in-eink-display.sh
@@ -262,7 +262,7 @@ fi
 
 # Download splash screen image
 cd /home/pi/hush-line
-wget https://raw.githubusercontent.com/scidsg/hush-line/main/static/images/splash.png
+wget https://raw.githubusercontent.com/scidsg/hushline-assets/main/images/splash.png
 
 echo "✅ E-ink display configuration complete. Rebooting your Raspberry Pi..."
 sleep 3
