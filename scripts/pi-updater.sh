e262d96a6aa415004281eabd8522a90172a7c8a8
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-08-26 16:34:07 -0700

    update updaters

---
+++ scripts/pi-updater.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Update and upgrade
+sudo apt update && sudo apt -y dist-upgrade && sudo apt -y autoremove
+
+# Function to display error message and exit
+error_exit() {
+    echo "An error occurred during installation. Please check the output above for more details."
+    exit 1
+}
+
+# Trap any errors and call the error_exit function
+trap error_exit ERR
+
+cd /home/pi/hush-line/templates
+mv index.html index.html.old
+wget https://raw.githubusercontent.com/scidsg/hushline/main/templates/index.html
+
+cd /home/pi/hush-line/static
+mv style.css style.css.old
+wget https://raw.githubusercontent.com/scidsg/hushline/main/static/style.css
+
+echo "
+✅ Update complete!
+                                               
+Hush Line is a product by Science & Design. 
+Learn more about us at https://scidsg.org.
+Have feedback? Send us an email at hushline@scidsg.org."
+
+sudo systemctl restart hush-line
+
+rm /home/pi/hush-line/static/style.css.old
+rm /home/pi/hush-line/templates/index.html.old
+
+# Disable the trap before exiting
+trap - ERR
