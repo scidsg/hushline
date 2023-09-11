4302a830a36d6261c8c0e4ab9a1c214132e88988
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-09-10 23:07:23 -0700

    Update waveshare-2_7in-eink-display-v1.sh

--- scripts/waveshare-2_7in-eink-display-v1.sh
+++ scripts/waveshare-2_7in-eink-display-v1.sh
@@ -32,7 +32,6 @@ pip3 install ./e-Paper/RaspberryPi_JetsonNano/python/
 pip3 install qrcode[pil]
 pip3 install requests python-gnupg
 
-
 # Install other Python packages
 pip3 install RPi.GPIO spidev
 apt-get -y autoremove
@@ -235,6 +234,33 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print('Exiting...')
         sys.exit(0)
+EOL
+
+# Create a new script to display status on the e-ink display
+cat > /home/pi/hush-line/clear_display.py << EOL
+import sys
+from waveshare_epd import epd2in7
+from PIL import Image
+
+def clear_screen(epd):
+    print("Clearing the screen")
+    image = Image.new('1', (epd.height, epd.width), 255)
+    image_rotated = image.rotate(90, expand=True)
+    epd.display(epd.getbuffer(image_rotated))
+    epd.sleep()
+
+def main():
+    print("Starting clear_display script")
+    epd = epd2in7.EPD()
+    epd.init()
+    clear_screen(epd)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        sys.exit(1)
 EOL
 
 # Clear display before shutdown
