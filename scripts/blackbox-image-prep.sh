63da3f465d0466f28aff477f69a6abac1a1e11ab
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-09-24 00:19:19 -0700

    Revert "Update blackbox-image-prep.sh"
    This reverts commit a527dba500ddac2eb16c2eed0e09f4bf01c06616.

--- scripts/blackbox-image-prep.sh
+++ scripts/blackbox-image-prep.sh
@@ -6,25 +6,6 @@ if [[ $EUID -ne 0 ]]; then
   exec sudo /bin/bash "$0" "$@"
 fi
 
-# Update and upgrade
-sudo apt update && sudo apt -y dist-upgrade && sudo apt -y autoremove
-
-# Install required packages
-sudo apt-get -y install git python3 python3-venv python3-pip nginx tor whiptail libnginx-mod-http-geoip geoip-database unattended-upgrades gunicorn libssl-dev net-tools jq
-
-# Install Waveshare e-Paper library
-pip3 install /home/hush/hushline/e-Paper/RaspberryPi_JetsonNano/python/
-pip3 install qrcode[pil]
-pip3 install requests python-gnupg
-
-# Install other Python packages
-pip3 install RPi.GPIO spidev
-
-pip3 install flask setuptools-rust pgpy gunicorn cryptography segno requests
-pip3 install -r requirements.txt
-
-# Clone the repositories
-
 git clone https://github.com/waveshare/e-Paper.git
 git clone https://github.com/scidsg/hushline.git
 
