7797ae80167cf8a5ab3899bb09d89f983f0e4c5f
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-09-25 21:43:46 -0700

    Update qr-installer-beta.sh

--- scripts/blackbox/v2/qr-installer-beta.sh
+++ scripts/blackbox/v2/qr-installer-beta.sh
@@ -342,4 +342,4 @@ deactivate
 # Disable the trap before exiting
 trap - ERR
 
-curl -sSL https://raw.githubusercontent.com/scidsg/hushline/main/scripts/blackbox/v1/waveshare-2_7in-qr-eink-display-v2-beta.sh | bash
+curl -sSL https://raw.githubusercontent.com/scidsg/hushline/main/scripts/blackbox/v2/waveshare-2_7in-qr-eink-display-v2-beta.sh | bash
