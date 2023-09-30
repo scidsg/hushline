faf3ef62732d9c7ba2ef5a84cf00d51add44d7ab
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-09-30 11:04:23 -0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,6 +21,9 @@ sudo apt update && sudo apt -y dist-upgrade && sudo apt -y autoremove
 # Install required packages
 sudo apt-get -y install whiptail curl git wget sudo
 
+# Clone the repository
+git clone https://github.com/scidsg/hushline.git
+
 # Welcome Prompt
 whiptail --title "🤫 Hush Line Installation" --msgbox "Hush Line provides a simple way to receive secure messages from sources, colleagues, clients, or patients.\n\nAfter installation, you'll have a private tip line hosted on your own server, secured with PGP, HTTPS, and available on a .onion address so anyone can message you, even from locations where the internet is censored.\n\nIf deploying to a public website, ensure your DNS settings point to this server." 16 64
 
@@ -32,9 +35,11 @@ exitstatus=$?
 if [ $exitstatus = 0 ]; then
     echo "Your chosen option:" $OPTION
     if [ $OPTION = "1" ]; then
-        curl -sSL https://raw.githubusercontent.com/scidsg/hushline/main/scripts/install-tor-only.sh | bash
+        chmod +x hushline/scripts/install-tor-only.sh
+        ./hushline/scripts/install-tor-only.sh
     elif [ $OPTION = "2" ]; then
-        curl -sSL https://raw.githubusercontent.com/scidsg/hushline/main/scripts/install-public-plus-tor.sh | bash
+        chmod +x hushline/scripts/install-public-plus-tor.sh
+        ./hushline/scripts/install-public-plus-tor.sh
     fi
 else
     echo "You chose Cancel."
