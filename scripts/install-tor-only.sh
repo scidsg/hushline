e7e00b706691bef9fa1bf6adeb9920c54ae9cb72
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-09-29 23:03:07 -0700

    Update install-tor-only.sh

--- scripts/install-tor-only.sh
+++ scripts/install-tor-only.sh
@@ -28,18 +28,19 @@ echo "
  | _ \/ __| _ \ | _ \ | | | _ ) |  |_ _/ __| | |/ / __\ \ / /
  |  _/ (_ |  _/ |  _/ |_| | _ \ |__ | | (__  | ' <| _| \ V / 
  |_|  \___|_|   |_|  \___/|___/____|___\___| |_|\_\___| |_|  
+<<<<<<< Updated upstream
  
 Please paste your public PGP key and then press Enter."
+=======
+>>>>>>> Stashed changes
 
+👇 Please paste your public PGP key. Once finished, type END on a new line and press Enter."
+
+# Read the PGP key
 PGP_PUBLIC_KEY=""
-end_delimiter="-----END PGP PUBLIC KEY BLOCK-----"
-
 while IFS= read -r LINE; do
+    [[ $LINE == "END" ]] && break
     PGP_PUBLIC_KEY+="$LINE"$'\n'
-    # If the end delimiter is detected, break out of the loop
-    if [[ "$LINE" == "$end_delimiter" ]]; then
-        break
-    fi
 done
 
 export DOMAIN
