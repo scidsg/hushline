618a376ad4dd53d21b878d96655eeba152366c21
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-10-02 20:29:51 -0700

    update paths

--- scripts/waveshare-2_7in-eink-display.sh
+++ scripts/waveshare-2_7in-eink-display.sh
@@ -204,10 +204,10 @@ def main():
     print("EPD initialized")
 
     # Display splash screen
-    splash_image_path = "/home/pi/hushline/splash.png"
+    splash_image_path = "/home/hush/hushline/splash.png"
     display_splash_screen(epd, splash_image_path, 3)
 
-    pgp_owner_info_url = "/home/pi/hushline/public_key.asc"
+    pgp_owner_info_url = "/home/hush/hushline/public_key.asc"
 
     try:
         while True:
@@ -237,7 +237,7 @@ if __name__ == '__main__':
 EOL
 
 # Create a new script to display status on the e-ink display
-cat >/home/pi/hushline/clear_display.py <<EOL
+cat >/home/hush/hushline/clear_display.py <<EOL
 import sys
 from waveshare_epd import epd2in7_V2
 from PIL import Image
@@ -272,7 +272,7 @@ Before=shutdown.target reboot.target halt.target
 
 [Service]
 Type=oneshot
-ExecStart=/usr/bin/python3 /home/pi/hushline/clear_display.py
+ExecStart=/usr/bin/python3 /home/hush/hushline/clear_display.py
 TimeoutStartSec=0
 
 [Install]
@@ -282,12 +282,12 @@ sudo systemctl daemon-reload
 sudo systemctl enable clear-display.service
 
 # Add a line to the .bashrc to run the display_status.py script on boot
-if ! grep -q "sudo python3 /home/hush/hushline/display_status.py" /home/pi/.bashrc; then
-    echo "sudo python3 /home/hush/hushline/display_status.py &" >>/home/pi/.bashrc
+if ! grep -q "sudo python3 /home/hush/hushline/display_status.py" /home/hush/.bashrc; then
+    echo "sudo python3 /home/hush/hushline/display_status.py &" >>/home/hush/.bashrc
 fi
 
 # Download splash screen image
-cd /home/pi/hushline
+cd /home/hush/hushline
 wget https://raw.githubusercontent.com/scidsg/hushline-assets/main/images/splash.png
 
 echo "✅ E-ink display configuration complete. Rebooting your Raspberry Pi..."
