78d203e77eaa78b1555a145d6e57f61a6d193f30
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-10-11 23:14:44 -0700

    update service name

--- scripts/install-public-plus-tor.sh
+++ scripts/install-public-plus-tor.sh
@@ -340,7 +340,7 @@ echo "display_status_indicator() {
 echo "display_status_indicator" >>/etc/bash.bashrc
 source /etc/bash.bashrc
 
-systemctl restart hush-line
+systemctl restart hushline
 
 rm -r $HOME/hushline/assets
 rm $HOME/hushline/scripts/install*
