0a6dd5d22d5c18586cdf15c12a1f53c24639d5ba
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-10-11 21:10:39 -0700

    Update install-tor-only.sh

--- scripts/install-tor-only.sh
+++ scripts/install-tor-only.sh
@@ -142,7 +142,7 @@ if [ -e "/etc/nginx/sites-enabled/default" ]; then
     rm /etc/nginx/sites-enabled/default
 fi
 ln -sf /etc/nginx/sites-available/hush-line.nginx /etc/nginx/sites-enabled/
-nginx -t && systemctl restart nginx || error_exit
+(nginx -t && systemctl restart nginx) || error_exit
 
 # System status indicator
 display_status_indicator() {
