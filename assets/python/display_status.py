f1aeb92319cc27ad58a5fcf08b16c7823b811161
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-10-22 12:15:52 -0700

    Update display_status.py

--- assets/python/display_status.py
+++ assets/python/display_status.py
@@ -9,10 +9,11 @@ import traceback
 from waveshare_epd import epd2in7_V2
 from PIL import Image, ImageDraw, ImageFont
 from PIL import ImageOps
+
 print(Image.__version__)
 
 def display_splash_screen(epd, image_path, display_time):
-    print(f'Displaying splash screen: {image_path}')
+    print(f"Displaying splash screen: {image_path}")
     image = Image.open(image_path).convert("L")
 
     target_height = int(epd.width * 0.75)
@@ -29,38 +30,49 @@ def display_splash_screen(epd, image_path, display_time):
     time.sleep(display_time)
     epd.init()
 
-def get_onion_address():
-    with open('/var/lib/tor/hidden_service/hostname', 'r') as f:
-        return f.read().strip()
+def get_local_address():
+    return "hushline.local/info"
 
 def get_service_status():
-    status = os.popen('systemctl is-active hushline.service').read().strip()
-    if status == 'active':
-        return '✔ Hush Line is running'
+    status = os.popen("systemctl is-active hushline.service").read().strip()
+    if status == "active":
+        return "✔ Hush Line is running"
     else:
-        return '⨯ Hush Line is not running'
+        return "⨯ Hush Line is not running"
 
-def display_status(epd, status, onion_address, name, email, key_id, expires):
-    print(f'Displaying status: {status}, Onion address: {onion_address}')
-    image = Image.new('1', (epd.height, epd.width), 255)
+def display_status(epd, status, local_address, name, email, key_id, expires):
+    print(f"Displaying status: {status}, Local address: {local_address}")
+    image = Image.new("1", (epd.height, epd.width), 255)
     draw = ImageDraw.Draw(image)
 
-    font_status = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 13)
+    font_status = ImageFont.truetype(
+        "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 13
+    )
 
     x_pos_status = 10
     y_pos_status = 10
     draw.text((x_pos_status, y_pos_status), status, font=font_status, fill=0)
 
     # Add the new text
-    font_instruction = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 11)
-    instruction_text = "Scan the QR code and open the link in Tor Browser to send a private message:"
+    font_instruction = ImageFont.truetype(
+        "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 11
+    )
+    instruction_text = (
+        "From the local wifi network, scan the QR code to learn about Hush Line:"
+    )
     y_pos_instruction = y_pos_status + font_status.getbbox(status)[3] + 7
     max_width = epd.height - 20
-    chars_per_line = max_width // font_instruction.getbbox('A')[2]
+    chars_per_line = max_width // font_instruction.getbbox("A")[2]
     wrapped_instruction = textwrap.wrap(instruction_text, width=40)
     for line in wrapped_instruction:
-        draw.text((x_pos_status, y_pos_instruction), line, font=font_instruction, fill=0)
-        y_pos_instruction += font_instruction.getbbox(wrapped_instruction[-1])[3] - font_instruction.getbbox(wrapped_instruction[-1])[1] + 5
+        draw.text(
+            (x_pos_status, y_pos_instruction), line, font=font_instruction, fill=0
+        )
+        y_pos_instruction += (
+            font_instruction.getbbox(wrapped_instruction[-1])[3]
+            - font_instruction.getbbox(wrapped_instruction[-1])[1]
+            + 5
+        )
 
     # Generate QR code
     qr = qrcode.QRCode(
@@ -69,21 +81,34 @@ def display_status(epd, status, onion_address, name, email, key_id, expires):
         box_size=3,
         border=2,
     )
-    qr.add_data(f'http://{onion_address}')
+    qr.add_data(f"http://{local_address}")
     qr.make(fit=True)
 
     qr_img = qr.make_image(fill_color="black", back_color="white")
 
     # Calculate available height for QR code
-    max_qr_height = epd.width - (y_pos_instruction + (font_instruction.getbbox(wrapped_instruction[-1])[3] - font_instruction.getbbox(wrapped_instruction[-1])[1]))
+    max_qr_height = epd.width - (
+        y_pos_instruction
+        + (
+            font_instruction.getbbox(wrapped_instruction[-1])[3]
+            - font_instruction.getbbox(wrapped_instruction[-1])[1]
+        )
+    )
 
     width_scale_factor = max_qr_height / qr_img.width
     height_scale_factor = max_qr_height / qr_img.height
 
-    new_size = (int(qr_img.width * width_scale_factor), int(qr_img.height * height_scale_factor))
+    new_size = (
+        int(qr_img.width * width_scale_factor),
+        int(qr_img.height * height_scale_factor),
+    )
     resized_qr_img = qr_img.resize(new_size, Image.NEAREST)
 
-    y_pos_instruction += font_instruction.getbbox(wrapped_instruction[-1])[3] - font_instruction.getbbox(wrapped_instruction[-1])[1] + 5
+    y_pos_instruction += (
+        font_instruction.getbbox(wrapped_instruction[-1])[3]
+        - font_instruction.getbbox(wrapped_instruction[-1])[1]
+        + 5
+    )
     x_pos = x_pos_status - 3
     y_pos = y_pos_instruction - 12
 
@@ -93,36 +118,42 @@ def display_status(epd, status, onion_address, name, email, key_id, expires):
     # Calculate the starting position for the PGP information text
     x_pos_info = x_pos + resized_qr_img.width + 10
 
-    font_info = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 11)
+    font_info = ImageFont.truetype(
+        "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 11
+    )
 
     # Change this line to a fixed value
     y_pos_info = 75  # initialize y_pos_info here before usage
 
-    font_info = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 11)
+    font_info = ImageFont.truetype(
+        "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 11
+    )
     # y_pos_info = y_pos_instruction + new_size[1] + 5  # initialize y_pos_info here before usage
 
     # Display the PGP owner information
     max_width = epd.height - x_pos_info - 5
-    chars_per_line = max_width // (font_info.getbbox('A')[2] - font_info.getbbox('A')[0])
-    
+    chars_per_line = max_width // (
+        font_info.getbbox("A")[2] - font_info.getbbox("A")[0]
+    )
+
     # Check if 'expires' is non-empty and numeric before converting
     if expires and expires.isdigit():
         expiry_date = time.strftime("%Y-%m-%d", time.gmtime(int(expires)))
     else:
         expiry_date = "Never"  # or some other appropriate default or message
 
-    pgp_info = f'{name} <{email}>\nKey ID: {key_id[-8:]}\nExp: {expiry_date}'
+    pgp_info = f"{name} <{email}>\nKey ID: {key_id[-8:]}\nExp: {expiry_date}"
 
     wrapped_pgp_info = []
 
-    for line in pgp_info.split('\n'):
+    for line in pgp_info.split("\n"):
         wrapped_pgp_info.extend(textwrap.wrap(line, width=chars_per_line))
 
     line_spacing = 2
     empty_line_spacing = 0
     for i, line in enumerate(wrapped_pgp_info):
         draw.text((x_pos_info, y_pos_info), line, font=font_info, fill=0)
-        if i < len(wrapped_pgp_info) - 1 and wrapped_pgp_info[i + 1] == '':
+        if i < len(wrapped_pgp_info) - 1 and wrapped_pgp_info[i + 1] == "":
             y_pos_info += font_info.getbbox(line)[3] + empty_line_spacing
         else:
             y_pos_info += font_info.getbbox(line)[3] + line_spacing
@@ -132,8 +163,9 @@ def display_status(epd, status, onion_address, name, email, key_id, expires):
 
     epd.display(epd.getbuffer(image_rotated))
 
+
 def get_pgp_owner_info(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         key_data = f.read()
 
     gpg = gnupg.GPG()
@@ -141,20 +173,22 @@ def get_pgp_owner_info(file_path):
     fingerprint = imported_key.fingerprints[0]
     key = gpg.list_keys(keys=fingerprint)[0]
 
-    uids = key['uids'][0].split()
-    name = ' '.join(uids[:-1])
-    email = uids[-1].strip('<>')
-    key_id = key['keyid']
-    expires = key['expires']
+    uids = key["uids"][0].split()
+    name = " ".join(uids[:-1])
+    email = uids[-1].strip("<>")
+    key_id = key["keyid"]
+    expires = key["expires"]
 
     return name, email, key_id, expires
 
+
 def clear_screen(epd):
     print("Clearing the screen")
-    image = Image.new('1', (epd.height, epd.width), 255)
+    image = Image.new("1", (epd.height, epd.width), 255)
     image_rotated = image.rotate(90, expand=True)
     epd.display(epd.getbuffer(image_rotated))
     epd.sleep()
+
 
 def main():
     print("Starting main function")
@@ -171,25 +205,26 @@ def main():
     try:
         while True:
             status = get_service_status()
-            print(f'Service status: {status}')
-            onion_address = get_onion_address()
-            print(f'Onion address: {onion_address}')
+            print(f"Service status: {status}")
+            local_address = get_local_address()
+            print(f"Local address: {local_address}")
             name, email, key_id, expires = get_pgp_owner_info(pgp_owner_info_url)
-            display_status(epd, status, onion_address, name, email, key_id, expires)
+            display_status(epd, status, local_address, name, email, key_id, expires)
             time.sleep(300)
     except KeyboardInterrupt:
         clear_screen(epd)
-        print('Exiting...')
+        print("Exiting...")
         sys.exit(0)
     except Exception:
         clear_screen(epd)
         print(f"Unexpected error:", traceback.format_exc())
         sys.exit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("Starting display_status script")
     try:
-            main()
+        main()
     except KeyboardInterrupt:
-        print('Exiting...')
+        print("Exiting...")
         sys.exit(0)
