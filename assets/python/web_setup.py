b9ddef799fc047903929e7a482da2ef8e450a327
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-10-23 22:28:57 -0700

    update py

--- assets/python/web_setup.py
+++ assets/python/web_setup.py
@@ -1,21 +1,30 @@
 from flask import Flask, request, render_template, redirect, url_for
 import json
 import os
+import smtplib
 import segno
-import requests
-import socket
 
 app = Flask(__name__)
 
 # Flag to indicate whether setup is complete
 setup_complete = os.path.exists('/tmp/setup_config.json')
 
+def test_smtp_credentials(email, password, server, port):
+    """Function to test SMTP credentials."""
+    try:
+        with smtplib.SMTP_SSL(server, int(port)) as smtp:
+            smtp.login(email, password)
+            return True
+    except:
+        return False
+
 @app.route('/setup', methods=['GET', 'POST'])
 def setup():
     global setup_complete
     if setup_complete:
         return redirect(url_for('index'))
-    
+
+    error_msg = None
     if request.method == 'POST':
         email = request.form.get('email')
         smtp_server = request.form.get('smtp_server')
@@ -23,25 +32,28 @@ def setup():
         smtp_port = request.form.get('smtp_port')
         pgp_public_key = request.form.get('pgp_public_key')
 
-        # Save the configuration
-        with open('/tmp/setup_config.json', 'w') as f:
-            json.dump({
-                'email': email,
-                'smtp_server': smtp_server,
-                'password': password,
-                'smtp_port': smtp_port,
-                'pgp_public_key': pgp_public_key
-            }, f)
+        if not test_smtp_credentials(email, password, smtp_server, smtp_port):
+            error_msg = "SMTP credentials are invalid. Please check your inputs."
+        else:
+            # Save the configuration
+            with open('/tmp/setup_config.json', 'w') as f:
+                json.dump({
+                    'email': email,
+                    'smtp_server': smtp_server,
+                    'password': password,
+                    'smtp_port': smtp_port,
+                    'pgp_public_key': pgp_public_key
+                }, f)
 
-        setup_complete = True
+            setup_complete = True
 
-        # Save the provided PGP key to a file
-        with open('/home/hush/hushline/public_key.asc', 'w') as keyfile:
-            keyfile.write(pgp_public_key)
+            # Save the provided PGP key to a file
+            with open('/home/hush/hushline/public_key.asc', 'w') as keyfile:
+                keyfile.write(pgp_public_key)
 
-        return redirect(url_for('index'))
+            return redirect(url_for('index'))
 
-    return render_template('setup.html')
+    return render_template('setup.html', error=error_msg)
 
 @app.route("/")
 def index():
