bd5f1e0a71eaec41d25dd73f557b26de4d45f14e
Glenn <glenn.sorrentino@proton.me>
2024-01-27 21:32:24 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1107,4 +1107,4 @@ def delete_account():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(port=5000)
