7eb81d4a778dae342e2a2fe0273c9d96654ff8ba
Glenn Sorrentino <git@scidsg.org>
2024-03-25 09:45:27 -0700

    Update Makefile and env variables

--- hushline/ext.py
+++ hushline/ext.py
@@ -6,8 +6,11 @@ from flask_limiter.util import get_remote_address
 
 bcrypt = Bcrypt()
 
+# Check for an environment variable or use in-memory storage as fallback
+REDIS_URI = os.getenv("REDIS_URI")
+
 limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["200 per day", "50 per hour"],
-    storage_uri=os.getenv("REDIS_URI", "memory://"),
+    storage_uri=REDIS_URI,
 )
