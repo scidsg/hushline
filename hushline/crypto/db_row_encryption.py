dda75778c8c5754d1642c7d93350b9ce126c946b
rmlibre <rmlibre@riseup.net>
2024-07-17 21:41:08 -0400

    refactor(crypto): provide clean interface parity [#411]

--- hushline/crypto/db_row_encryption.py
+++ hushline/crypto/db_row_encryption.py
@@ -12,16 +12,14 @@ def encrypt_field(
     if data is None:
         return None
 
-    # Check if data is already a bytes object
     if not isinstance(data, bytes):
-        # If data is a string, encode it to bytes
         data = data.encode()
 
     return current_app.config["VAULT"].encrypt(data, domain=domain, aad=aad).decode()
 
 
 def decrypt_field(
-    data: str | None,
+    data: bytes | str | None,
     *,
     domain: bytes | bytearray,
     aad: bytes | bytearray = b"",
@@ -29,6 +27,8 @@ def decrypt_field(
 ) -> str | None:
     if data is None:
         return None
-    return current_app.config["VAULT"].decrypt(
-        data.encode(), domain=domain, aad=aad, ttl=ttl
-    ).decode()
+
+    if not isinstance(data, bytes):
+        data = data.encode()
+
+    return current_app.config["VAULT"].decrypt(data, domain=domain, aad=aad, ttl=ttl).decode()
