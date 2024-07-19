7f46de55ccac7106bdcc61db7c6ca1143eb5389d
rmlibre <rmlibre@riseup.net>
2024-07-18 21:15:13 -0400

    test(crypto): make fast app fixtures for new row encryption tests [#411]

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,3 +48,48 @@ def app(
 def client(app: Flask) -> Generator[FlaskClient, None, None]:
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture(scope="session")
+def session_monkeypatch(
+    request: pytest.FixtureRequest,
+) -> Generator[pytest.MonkeyPatch, None, None]:
+    monkeypatch = pytest.MonkeyPatch()
+
+    yield monkeypatch
+
+    monkeypatch.undo
+
+
+@pytest.fixture(params=["environment", "interactive"], scope="session")
+def static_app(
+    session_monkeypatch: pytest.MonkeyPatch, request: pytest.FixtureRequest
+) -> Generator[Flask, None, None]:
+    """A faster app fixture. Should only be used in tests which *do not* leave
+    the environment or app state mutated from its initial state.
+    """
+    os.environ["ADMIN_INPUT_SOURCE"] = request.param
+    os.environ["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    os.environ["SQLALCHEMY_TRACK_MODIFICATIONS"] = "False"
+    os.environ["REGISTRATION_CODES_REQUIRED"] = "False"
+
+    def mock_getpass(prompt: str) -> str:
+        return TEST_ADMIN_SECRET.hex()
+
+    session_monkeypatch.setattr("getpass.getpass", mock_getpass)
+    app = create_app()
+    app.config["TESTING"] = True
+    app.config["WTF_CSRF_ENABLED"] = False
+    app.config["SERVER_NAME"] = "localhost:8080"
+    app.config["PREFERRED_URL_SCHEME"] = "http"
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all()
+
+
+@pytest.fixture(scope="session")
+def static_client(static_app: Flask) -> Generator[FlaskClient, None, None]:
+    with static_app.test_client() as client:
+        yield client
---
+++ tests/test_db_row_encryption_interface.py
@@ -0,0 +1,29 @@
+import pytest
+from conftest import Flask
+
+from hushline.crypto.db_row_encryption import decrypt_field, encrypt_field
+
+
+@pytest.mark.parametrize("data_size_multiple", [0, 1, 32])
+@pytest.mark.parametrize("aad", [b"timestamp", bytearray(b"timestamp")])
+@pytest.mark.parametrize("domain", [b"totp_secret", bytearray(b"totp_secret")])
+@pytest.mark.parametrize("data", [b"example data", "example data"])
+def test_declared_row_value_types_are_handled_correctly(
+    data: bytes | str,
+    domain: bytes | bytearray,
+    aad: bytes | bytearray,
+    data_size_multiple: int,
+    static_app: Flask,
+) -> None:
+    with static_app.app_context():
+        assert encrypt_field(None, domain=domain, aad=aad) is None
+        assert decrypt_field(None, domain=domain, aad=aad) is None
+
+        plaintext = data_size_multiple * data
+        ciphertext = encrypt_field(plaintext, domain=domain, aad=aad)
+
+        if isinstance(plaintext, bytes):
+            plaintext = plaintext.decode()
+        assert isinstance(ciphertext, str)
+        assert plaintext == decrypt_field(ciphertext, domain=domain, aad=aad)
+        assert plaintext == decrypt_field(ciphertext.encode(), domain=domain, aad=aad)
