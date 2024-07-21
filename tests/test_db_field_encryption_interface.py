d1090f7b0783804a856443860bb397f50a6d5885
rmlibre <rmlibre@riseup.net>
2024-07-21 13:06:47 -0400

    refactor(test): use term 'field' instead of 'row' [#411]

--- tests/test_db_field_encryption_interface.py
+++ tests/test_db_field_encryption_interface.py
@@ -8,7 +8,7 @@ from hushline.crypto.db_field_encryption import decrypt_field, encrypt_field
 @pytest.mark.parametrize("aad", [b"timestamp", bytearray(b"timestamp")])
 @pytest.mark.parametrize("domain", [b"totp_secret", bytearray(b"totp_secret")])
 @pytest.mark.parametrize("data", [b"example data", "example data"])
-def test_declared_row_value_types_are_handled_correctly(
+def test_declared_field_value_types_are_handled_correctly(
     data: bytes | str,
     domain: bytes | bytearray,
     aad: bytes | bytearray,
