5dbdc9623f7aee6b4a16221b31ec0bd853f67ce6
rmlibre <rmlibre@riseup.net>
2024-07-22 12:03:54 -0400

    test(crypto): extend tests to more declared 'aad' types [#411]

--- tests/test_db_field_encryption_interface.py
+++ tests/test_db_field_encryption_interface.py
@@ -10,32 +10,37 @@ from hushline.crypto.db_field_encryption import decrypt_field, encrypt_field
 
 
 @pytest.mark.parametrize("data_size_multiple", [0, 1, 32])
-@pytest.mark.parametrize("aad", [deque([b"timestamp"]), deque([bytearray(b"timestamp")])])
+@pytest.mark.parametrize("aad", [None, deque([b"timestamp"]), deque([bytearray(b"timestamp")])])
 @pytest.mark.parametrize("domain", [b"totp_secret", bytearray(b"totp_secret")])
 @pytest.mark.parametrize("data", [b"example data", "example data"])
 def test_declared_field_value_types_are_handled_correctly(
     data: bytes | str,
     domain: bytes | bytearray,
-    aad: deque[bytes | bytearray],
+    aad: deque[bytes | bytearray] | None,
     data_size_multiple: int,
     static_app: Flask,
 ) -> None:
+    def new_aad() -> deque[bytes | bytearray] | None:
+        if aad is None:
+            return None
+        return aad.copy()
+
     with static_app.app_context():
-        assert encrypt_field(None, domain=domain, aad=aad.copy()) is None
-        assert decrypt_field(None, domain=domain, aad=aad.copy()) is None
+        assert encrypt_field(None, domain=domain, aad=new_aad()) is None
+        assert decrypt_field(None, domain=domain, aad=new_aad()) is None
 
         plaintext = data_size_multiple * data
-        ciphertext = encrypt_field(plaintext, domain=domain, aad=aad.copy())
+        ciphertext = encrypt_field(plaintext, domain=domain, aad=new_aad())
 
         if isinstance(plaintext, bytes):
             plaintext = plaintext.decode()
         assert isinstance(plaintext, str)
         assert isinstance(ciphertext, str)
-        assert plaintext == decrypt_field(ciphertext, domain=domain, aad=aad.copy())
-        assert plaintext == decrypt_field(ciphertext.encode(), domain=domain, aad=aad.copy())
+        assert plaintext == decrypt_field(ciphertext, domain=domain, aad=new_aad())
+        assert plaintext == decrypt_field(ciphertext.encode(), domain=domain, aad=new_aad())
 
 
-@pytest.mark.parametrize("aad", [deque([bytearray(token_bytes(16))]) for _ in range(4)])
+@pytest.mark.parametrize("aad", [deque([token_bytes(16), bytearray(token_bytes(16))])])
 @pytest.mark.parametrize("domain", [b"password_hash", bytearray(b"totp_secret")])
 @pytest.mark.parametrize("data", [urlsafe_b64encode(token_bytes(8)), token_bytes(8).hex()])
 def test_aad_deque_is_cleared_but_aad_items_are_not_mutated(
