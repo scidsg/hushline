c3ed5f448ac1a33fc0259563a62eb8bfb396c404
rmlibre <rmlibre@riseup.net>
2024-07-22 20:49:05 -0400

    build: add Ruff formatter commands to 'Makefile' devtools & run them

--- hushline/routes.py
+++ hushline/routes.py
@@ -422,7 +422,7 @@ def init_app(app: Flask) -> None:
         logged_in = "user_id" in session
         return render_template("directory.html", users=get_directory_users(), logged_in=logged_in)
 
-    @app.route('/directory/get-session-user.json')
+    @app.route("/directory/get-session-user.json")
     def session_user() -> dict[str, bool]:
         logged_in = "user_id" in session
         return {"logged_in": logged_in}
