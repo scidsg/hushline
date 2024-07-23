a9e2222a97b58b56d9bac1306378fab2920a9a83
rmlibre <rmlibre@riseup.net>
2024-07-22 23:43:15 -0400

    docs(crypto): remove deprecated password hash comment [#447]
    Suggested in review:
    https://github.com/scidsg/hushline/pull/447#pullrequestreview-2192892078
    
    Co-authored-by: Jeremy Moore <jeremymoore@gmail.com>

--- hushline/settings.py
+++ hushline/settings.py
@@ -177,7 +177,6 @@ def create_blueprint() -> Blueprint:
             # Handle Change Password Form Submission
             if change_password_form.validate_on_submit():
                 if user.check_password(change_password_form.old_password.data):
-                    # Hash the new password using scrypt and update the user object
                     user.password_hash = change_password_form.new_password.data
                     db.session.commit()
                     flash("👍 Password changed successfully.")
