e904cee743999a342bf5e85fad1a4cd041e28fc8
brassy endomorph <brassy-endomorph@riseup.net>
2024-09-10 21:03:06 +0000

    configure tests to use postgres

--- dev_env.sh
+++ dev_env.sh
@@ -1,7 +1,7 @@
 export FLASK_APP=hushline
 export ENCRYPTION_KEY=bi5FDwhZGKfc4urLJ_ChGtIAaOPgxd3RDOhnvct10mw=
 export SECRET_KEY=cb3f4afde364bfb3956b97ca22ef4d2b593d9d980a4330686267cabcd2c0befd
-export SQLALCHEMY_DATABASE_URI=sqlite:///hushline.db
+export SQLALCHEMY_DATABASE_URI=postgresql://hushline:hushline@127.0.0.1:5432/hushline
 export REGISTRATION_CODES_REQUIRED=False
 export SESSION_COOKIE_NAME=session
 export NOTIFICATIONS_ADDRESS=notifications@hushline.app
