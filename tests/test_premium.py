e3a1e107476b1c48ccf6177f250a180fb5f87ee3
Micah Lee <micah@micahflee.com>
2024-09-18 15:12:16 -0700

    Add some test data, and start figuring out the tests to write

--- tests/test_premium.py
+++ tests/test_premium.py
@@ -1,8 +1,24 @@
-# from flask.testing import FlaskClient
+from flask.testing import FlaskClient
 
-# from hushline.db import db
-# from hushline.model import Message
+from hushline.db import db
+from hushline.model import StripeEvent
 
 
-# def test_webhook_customer_subscription_created(client: FlaskClient) -> None:
-#     pass
+def test_webhook_customer_subscription_created(client: FlaskClient) -> None:
+    pass
+
+
+def test_webhook_customer_subscription_updated(client: FlaskClient) -> None:
+    pass
+
+
+def test_webhook_customer_subscription_deleted(client: FlaskClient) -> None:
+    pass
+
+
+def test_webhook_invoice_created(client: FlaskClient) -> None:
+    pass
+
+
+def test_webhook_invoice_payment_succeeded(client: FlaskClient) -> None:
+    pass
