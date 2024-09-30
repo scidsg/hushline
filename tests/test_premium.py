3b1c02e1841b6d536690fec338d580c0bfa5675c
Micah Lee <micah@micahflee.com>
2024-09-30 16:19:56 -0700

    Remove unnecessary context managers

--- tests/test_premium.py
+++ tests/test_premium.py
@@ -45,272 +45,261 @@ def business_tier() -> Tier:
 
 
 def test_create_products_and_prices(app: Flask, mocker: MagicMock) -> None:
-    with app.app_context():
-        # Remove the stripe_product_id and stripe_price_id from the Business tier
-        tier = Tier.query.filter_by(name="Business").first()
-        assert tier is not None
-        if not tier:
-            return
+    # Remove the stripe_product_id and stripe_price_id from the Business tier
+    tier = Tier.query.filter_by(name="Business").first()
+    assert tier is not None
+    if not tier:
+        return
 
-        tier.stripe_product_id = None
-        tier.stripe_price_id = None
-        db.session.add(tier)
-        db.session.commit()
+    tier.stripe_product_id = None
+    tier.stripe_price_id = None
+    db.session.add(tier)
+    db.session.commit()
 
-        stripe_error_instance = MagicMock()
-        stripe_error_instance.side_effect = stripe._error.InvalidRequestError("", param={})
+    stripe_error_instance = MagicMock()
+    stripe_error_instance.side_effect = stripe._error.InvalidRequestError("", param={})
 
-        # Mock the Stripe API calls
-        mock_stripe_product_create = mocker.patch(
-            "hushline.premium.stripe.Product.create", return_value=MagicMock(id="prod_123")
-        )
-        mock_stripe_price_create = mocker.patch(
-            "hushline.premium.stripe.Price.create", return_value=MagicMock(id="price_123")
-        )
-        mock_stripe_product_retrieve = mocker.patch(
-            "hushline.premium.stripe.Product.retrieve",
-            side_effect=stripe_error_instance,
-        )
-        mocker.patch(
-            "hushline.premium.stripe.Product.list",
-            return_value=[MagicMock(id="prod_123", name="Business")],
-        )
-        mock_stripe_price_retrieve = mocker.patch(
-            "hushline.premium.stripe.Price.retrieve",
-            side_effect=stripe_error_instance,
-        )
+    # Mock the Stripe API calls
+    mock_stripe_product_create = mocker.patch(
+        "hushline.premium.stripe.Product.create", return_value=MagicMock(id="prod_123")
+    )
+    mock_stripe_price_create = mocker.patch(
+        "hushline.premium.stripe.Price.create", return_value=MagicMock(id="price_123")
+    )
+    mock_stripe_product_retrieve = mocker.patch(
+        "hushline.premium.stripe.Product.retrieve",
+        side_effect=stripe_error_instance,
+    )
+    mocker.patch(
+        "hushline.premium.stripe.Product.list",
+        return_value=[MagicMock(id="prod_123", name="Business")],
+    )
+    mock_stripe_price_retrieve = mocker.patch(
+        "hushline.premium.stripe.Price.retrieve",
+        side_effect=stripe_error_instance,
+    )
 
-        create_products_and_prices()
+    create_products_and_prices()
 
-        # Check that the product creation was called
-        assert mock_stripe_product_create.called
-        assert mock_stripe_price_create.called
+    # Check that the product creation was called
+    assert mock_stripe_product_create.called
+    assert mock_stripe_price_create.called
 
-        # Check that stripe_product_id and stripe_price_id were set
-        tier = Tier.query.filter_by(name="Business").first()
-        assert tier is not None
-        if not tier:
-            return
+    # Check that stripe_product_id and stripe_price_id were set
+    tier = Tier.query.filter_by(name="Business").first()
+    assert tier is not None
+    if not tier:
+        return
 
-        assert tier.stripe_product_id == "prod_123"
-        assert tier.stripe_price_id == "price_123"
+    assert tier.stripe_product_id == "prod_123"
+    assert tier.stripe_price_id == "price_123"
 
-        # Test the case where the product already exists in Stripe
+    # Test the case where the product already exists in Stripe
 
-        tier.stripe_product_id = "prod_123"
-        tier.stripe_price_id = None
-        db.session.add(tier)
-        db.session.commit()
+    tier.stripe_product_id = "prod_123"
+    tier.stripe_price_id = None
+    db.session.add(tier)
+    db.session.commit()
 
-        mock_stripe_product_retrieve.side_effect = None
-        mock_stripe_product_retrieve.return_value = MagicMock(id="prod_123")
-        mock_stripe_price_retrieve.side_effect = stripe_error_instance
+    mock_stripe_product_retrieve.side_effect = None
+    mock_stripe_product_retrieve.return_value = MagicMock(id="prod_123")
+    mock_stripe_price_retrieve.side_effect = stripe_error_instance
 
-        create_products_and_prices()
+    create_products_and_prices()
 
-        # Check that the product creation was not called again
-        assert mock_stripe_product_create.call_count == 1
-        assert mock_stripe_price_create.call_count == 2
+    # Check that the product creation was not called again
+    assert mock_stripe_product_create.call_count == 1
+    assert mock_stripe_price_create.call_count == 2
 
-        # Check that stripe_product_id and stripe_price_id were set
-        tier = Tier.query.filter_by(name="Business").first()
-        assert tier is not None
-        if not tier:
-            return
+    # Check that stripe_product_id and stripe_price_id were set
+    tier = Tier.query.filter_by(name="Business").first()
+    assert tier is not None
+    if not tier:
+        return
 
-        assert tier.stripe_product_id == "prod_123"
-        assert tier.stripe_price_id == "price_123"
+    assert tier.stripe_product_id == "prod_123"
+    assert tier.stripe_price_id == "price_123"
 
 
 def test_update_price_existing(app: Flask, mock_stripe: MagicMock) -> None:
     mock_stripe.Price.search.return_value = [MagicMock(id="price_123", unit_amount=2000)]
 
-    with app.app_context():
-        tier = Tier.query.filter_by(name="Business").first()
-        assert tier is not None
-        if not tier:
-            return
+    tier = Tier.query.filter_by(name="Business").first()
+    assert tier is not None
+    if not tier:
+        return
 
-        update_price(tier)
+    update_price(tier)
 
-        assert not mock_stripe.Price.create.called
-        assert mock_stripe.Product.modify.called
+    assert not mock_stripe.Price.create.called
+    assert mock_stripe.Product.modify.called
 
 
 def test_update_price_new(app: Flask, mock_stripe: MagicMock) -> None:
     mock_stripe.Price.search.return_value = []
     mock_stripe.Price.create.return_value = MagicMock(id="price_123")
 
-    with app.app_context():
-        tier = Tier.query.filter_by(name="Business").first()
-        assert tier is not None
-        if not tier:
-            return
+    tier = Tier.query.filter_by(name="Business").first()
+    assert tier is not None
+    if not tier:
+        return
 
-        update_price(tier)
+    update_price(tier)
 
-        assert mock_stripe.Price.create.called
-        assert mock_stripe.Product.modify.called
+    assert mock_stripe.Price.create.called
+    assert mock_stripe.Product.modify.called
 
 
 def test_create_customer(app: Flask, mock_stripe: MagicMock) -> None:
     mock_stripe.Customer.create.return_value = MagicMock(id="cus_123")
 
-    with app.app_context():
-        user = User(email="test@example.com", password="password")  # noqa: S106
-        db.session.add(user)
-        db.session.commit()
+    user = User(email="test@example.com", password="password")  # noqa: S106
+    db.session.add(user)
+    db.session.commit()
 
-        create_customer(user)
+    create_customer(user)
 
-        assert mock_stripe.Customer.create.called
-        assert user.stripe_customer_id is not None
+    assert mock_stripe.Customer.create.called
+    assert user.stripe_customer_id is not None
 
-        # Check that the customer ID was saved to the database
-        db.session.refresh(user)
-        assert user.stripe_customer_id is not None
+    # Check that the customer ID was saved to the database
+    db.session.refresh(user)
+    assert user.stripe_customer_id is not None
 
 
 def test_get_subscription(app: Flask, mock_stripe: MagicMock) -> None:
     mock_stripe.Subscription.retrieve.return_value = MagicMock(id="sub_123")
 
-    with app.app_context():
-        user = User(email="test@example.com", password="password")  # noqa: S106
-        user.stripe_subscription_id = "sub_123"
-        db.session.add(user)
-        db.session.commit()
+    user = User(email="test@example.com", password="password")  # noqa: S106
+    user.stripe_subscription_id = "sub_123"
+    db.session.add(user)
+    db.session.commit()
 
-        subscription = get_subscription(user)
+    subscription = get_subscription(user)
 
-        assert subscription is not None
-        assert mock_stripe.Subscription.retrieve.called
+    assert subscription is not None
+    assert mock_stripe.Subscription.retrieve.called
 
 
 def test_handle_subscription_created(app: Flask) -> None:
-    with app.app_context():
-        user = User(email="test@example.com", password="password")  # noqa: S106
-        user.stripe_customer_id = "cus_123"
-        db.session.add(user)
-        db.session.commit()
+    user = User(email="test@example.com", password="password")  # noqa: S106
+    user.stripe_customer_id = "cus_123"
+    db.session.add(user)
+    db.session.commit()
 
-        subscription = MagicMock()
-        subscription.customer = "cus_123"
-        subscription.id = "sub_123"
-        subscription.status = StripeSubscriptionStatusEnum.INCOMPLETE.value
-        subscription.cancel_at_period_end = False
-        subscription.current_period_end = (datetime.now() + timedelta(days=30)).timestamp()
-        subscription.current_period_start = datetime.now().timestamp()
+    subscription = MagicMock()
+    subscription.customer = "cus_123"
+    subscription.id = "sub_123"
+    subscription.status = StripeSubscriptionStatusEnum.INCOMPLETE.value
+    subscription.cancel_at_period_end = False
+    subscription.current_period_end = (datetime.now() + timedelta(days=30)).timestamp()
+    subscription.current_period_start = datetime.now().timestamp()
 
-        handle_subscription_created(subscription)
+    handle_subscription_created(subscription)
 
-        assert user.stripe_subscription_id == "sub_123"
-        assert user.tier_id != BUSINESS_TIER
+    assert user.stripe_subscription_id == "sub_123"
+    assert user.tier_id != BUSINESS_TIER
 
 
 def test_handle_subscription_updated_upgrade(app: Flask) -> None:
-    with app.app_context():
-        user = User(email="test@example.com", password="password")  # noqa: S106
-        user.stripe_subscription_id = "sub_123"
-        db.session.add(user)
-        db.session.commit()
+    user = User(email="test@example.com", password="password")  # noqa: S106
+    user.stripe_subscription_id = "sub_123"
+    db.session.add(user)
+    db.session.commit()
 
-        subscription = MagicMock()
-        subscription.id = "sub_123"
-        subscription.status = StripeSubscriptionStatusEnum.ACTIVE.value
-        subscription.cancel_at_period_end = False
-        subscription.current_period_end = (datetime.now() + timedelta(days=30)).timestamp()
-        subscription.current_period_start = datetime.now().timestamp()
+    subscription = MagicMock()
+    subscription.id = "sub_123"
+    subscription.status = StripeSubscriptionStatusEnum.ACTIVE.value
+    subscription.cancel_at_period_end = False
+    subscription.current_period_end = (datetime.now() + timedelta(days=30)).timestamp()
+    subscription.current_period_start = datetime.now().timestamp()
 
-        handle_subscription_updated(subscription)
+    handle_subscription_updated(subscription)
 
-        assert user.tier_id == BUSINESS_TIER
+    assert user.tier_id == BUSINESS_TIER
 
 
 def test_handle_subscription_updated_downgrade(app: Flask) -> None:
-    with app.app_context():
-        user = User(email="test@example.com", password="password")  # noqa: S106
-        user.stripe_subscription_id = "sub_123"
-        db.session.add(user)
-        db.session.commit()
+    user = User(email="test@example.com", password="password")  # noqa: S106
+    user.stripe_subscription_id = "sub_123"
+    db.session.add(user)
+    db.session.commit()
 
-        subscription = MagicMock()
-        subscription.id = "sub_123"
-        subscription.status = StripeSubscriptionStatusEnum.CANCELED.value
-        subscription.cancel_at_period_end = True
-        subscription.current_period_end = (datetime.now() + timedelta(days=30)).timestamp()
-        subscription.current_period_start = datetime.now().timestamp()
+    subscription = MagicMock()
+    subscription.id = "sub_123"
+    subscription.status = StripeSubscriptionStatusEnum.CANCELED.value
+    subscription.cancel_at_period_end = True
+    subscription.current_period_end = (datetime.now() + timedelta(days=30)).timestamp()
+    subscription.current_period_start = datetime.now().timestamp()
 
-        handle_subscription_updated(subscription)
+    handle_subscription_updated(subscription)
 
-        assert user.tier_id == FREE_TIER
+    assert user.tier_id == FREE_TIER
 
 
 def test_handle_subscription_deleted(app: Flask) -> None:
-    with app.app_context():
-        user = User(email="test@example.com", password="password")  # noqa: S106
-        user.stripe_subscription_id = "sub_123"
-        db.session.add(user)
-        db.session.commit()
+    user = User(email="test@example.com", password="password")  # noqa: S106
+    user.stripe_subscription_id = "sub_123"
+    db.session.add(user)
+    db.session.commit()
 
-        subscription = MagicMock()
-        subscription.id = "sub_123"
+    subscription = MagicMock()
+    subscription.id = "sub_123"
 
-        handle_subscription_deleted(subscription)
+    handle_subscription_deleted(subscription)
 
-        assert user.tier_id == FREE_TIER
-        assert user.stripe_subscription_id is None
+    assert user.tier_id == FREE_TIER
+    assert user.stripe_subscription_id is None
 
 
 def test_handle_invoice_created(app: Flask) -> None:
-    with app.app_context():
-        user = User(email="test@example.com", password="password")  # noqa: S106
-        user.stripe_customer_id = "cus_123"
-        db.session.add(user)
-        db.session.commit()
+    user = User(email="test@example.com", password="password")  # noqa: S106
+    user.stripe_customer_id = "cus_123"
+    db.session.add(user)
+    db.session.commit()
 
-        handle_invoice_created(
-            MagicMock(
-                id="inv_123",
-                customer="cus_123",
-                hosted_invoice_url="https://example.com",
-                total=2000,
-                status=StripeInvoiceStatusEnum.OPEN,
-                lines=MagicMock(data=[MagicMock(plan=MagicMock(product="prod_123"))]),
-            )
-        )
-
-        stripe_invoice = db.session.query(StripeInvoice).filter_by(invoice_id="inv_123").first()
-        assert stripe_invoice is not None
-
-
-def test_handle_invoice_updated(app: Flask) -> None:
-    with app.app_context():
-        user = User(email="test@example.com", password="password")  # noqa: S106
-        user.stripe_customer_id = "cus_123"
-        user.stripe_subscription_id = "sub_123"
-        db.session.add(user)
-        db.session.commit()
-
-        invoice = MagicMock(
+    handle_invoice_created(
+        MagicMock(
             id="inv_123",
             customer="cus_123",
             hosted_invoice_url="https://example.com",
             total=2000,
             status=StripeInvoiceStatusEnum.OPEN,
             lines=MagicMock(data=[MagicMock(plan=MagicMock(product="prod_123"))]),
-            subscription="sub_123",
         )
-        handle_invoice_created(invoice)
+    )
 
-        stripe_invoice = db.session.query(StripeInvoice).filter_by(invoice_id="inv_123").first()
-        assert stripe_invoice is not None
+    stripe_invoice = db.session.query(StripeInvoice).filter_by(invoice_id="inv_123").first()
+    assert stripe_invoice is not None
 
-        invoice.status = StripeInvoiceStatusEnum.PAID
-        handle_invoice_updated(invoice)
 
-        stripe_invoice = db.session.query(StripeInvoice).filter_by(invoice_id="inv_123").first()
-        assert stripe_invoice is not None
-        assert stripe_invoice.status == StripeInvoiceStatusEnum.PAID
+def test_handle_invoice_updated(app: Flask) -> None:
+    user = User(email="test@example.com", password="password")  # noqa: S106
+    user.stripe_customer_id = "cus_123"
+    user.stripe_subscription_id = "sub_123"
+    db.session.add(user)
+    db.session.commit()
+
+    invoice = MagicMock(
+        id="inv_123",
+        customer="cus_123",
+        hosted_invoice_url="https://example.com",
+        total=2000,
+        status=StripeInvoiceStatusEnum.OPEN,
+        lines=MagicMock(data=[MagicMock(plan=MagicMock(product="prod_123"))]),
+        subscription="sub_123",
+    )
+    handle_invoice_created(invoice)
+
+    stripe_invoice = db.session.query(StripeInvoice).filter_by(invoice_id="inv_123").first()
+    assert stripe_invoice is not None
+
+    invoice.status = StripeInvoiceStatusEnum.PAID
+    handle_invoice_updated(invoice)
+
+    stripe_invoice = db.session.query(StripeInvoice).filter_by(invoice_id="inv_123").first()
+    assert stripe_invoice is not None
+    assert stripe_invoice.status == StripeInvoiceStatusEnum.PAID
 
 
 @pytest.mark.usefixtures("_authenticated_user")
