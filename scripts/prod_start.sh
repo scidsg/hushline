1c64d4ee8631e91b856aef2b272dae098f6e6323
Micah Lee <micah@micahflee.com>
2024-10-07 12:15:28 -0700

    Add debug output to prod_start.sh, so it is easier to ensure each step is occuring

--- scripts/prod_start.sh
+++ scripts/prod_start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Run migrations
+echo "> Running migrations"
 poetry run flask db upgrade
 
 # Configure Stripe and tiers
+echo "> Configuring Stripe"
 poetry run flask stripe configure
 
 # Start the server
+echo "> Starting the server"
 poetry run gunicorn "hushline:create_app()" -b 0.0.0.0:8080
