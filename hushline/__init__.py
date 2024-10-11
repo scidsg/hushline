1e47f2bb245792212d49207415c93dc4f5a154b7
brassy endomorph <brassy-endomorph@riseup.net>
2024-10-11 08:12:40 +0000

    move config parsing to own module

--- hushline/__init__.py
+++ hushline/__init__.py
@@ -1,80 +1,68 @@
 import asyncio
 import logging
-import os
-from datetime import timedelta
-from typing import Any
+from typing import Any, Mapping, Optional
 
 from flask import Flask, flash, redirect, request, session, url_for
 from flask.cli import AppGroup
 from jinja2 import StrictUndefined
-from markupsafe import Markup
 from werkzeug.wrappers.response import Response
 
 from . import admin, premium, routes, settings
+from .config import load_config
 from .db import db, migrate
 from .model import HostOrganization, Tier, User
 from .version import __version__
 
 
-def create_app() -> Flask:
+def create_app(config: Optional[Mapping[str, Any]] = None) -> Flask:
     app = Flask(__name__)
-
-    # Configure logging
     app.logger.setLevel(logging.DEBUG)
 
-    # sqlalchemy configs
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    # if it's a Postgres URI, replace the scheme with `postgresql+psycopg`
-    # because we're using the psycopg driver
-    if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgresql://"):
-        app.config["SQLALCHEMY_DATABASE_URI"] = app.config["SQLALCHEMY_DATABASE_URI"].replace(
-            "postgresql://", "postgresql+psycopg://", 1
-        )
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    # cookie configs
-    app.config["FLASK_ENV"] = os.environ.get("FLASK_ENV")
-    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-    app.config["SESSION_COOKIE_NAME"] = os.environ.get("SESSION_COOKIE_NAME", "__HOST-session")
-    app.config["SESSION_COOKIE_SECURE"] = True
-    app.config["SESSION_COOKIE_HTTPONLY"] = True
-    app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
-    app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30)
+    if not config:
+        config = load_config()
 
     # hushline specific configs
-    app.config["VERSION"] = __version__
-    app.config["ENCRYPTION_KEY"] = os.getenv("ENCRYPTION_KEY")
-    app.config["ONION_HOSTNAME"] = os.environ.get("ONION_HOSTNAME", None)
-    app.config["DIRECTORY_VERIFIED_TAB_ENABLED"] = (
-        os.environ.get("DIRECTORY_VERIFIED_TAB_ENABLED", "true").lower() == "true"
-    )
-    app.config["SMTP_FORWARDING_MESSAGE_HTML"] = (
-        Markup(os.environ.get("SMTP_FORWARDING_MESSAGE_HTML", "")) or None
-    )
-    app.config["REGISTRATION_CODES_REQUIRED"] = (
-        os.environ.get("REGISTRATION_CODES_REQUIRED", "true").lower() == "true"
-    )
-    app.config["NOTIFICATIONS_ADDRESS"] = os.environ.get("NOTIFICATIONS_ADDRESS", None)
-    app.config["SMTP_USERNAME"] = os.environ.get("SMTP_USERNAME", None)
-    app.config["SMTP_PASSWORD"] = os.environ.get("SMTP_PASSWORD", None)
-    app.config["SMTP_SERVER"] = os.environ.get("SMTP_SERVER", None)
-    app.config["SMTP_PORT"] = int(os.environ.get("SMTP_PORT", 0))
-    app.config["SMTP_ENCRYPTION"] = os.environ.get("SMTP_ENCRYPTION", "StartTLS")
-    app.config["REQUIRE_PGP"] = os.environ.get("REQUIRE_PGP", "False").lower() == "true"
-    app.config["STRIPE_PUBLISHABLE_KEY"] = os.environ.get("STRIPE_PUBLISHABLE_KEY", None)
-    app.config["STRIPE_SECRET_KEY"] = os.environ.get("STRIPE_SECRET_KEY", None)
-    app.config["STRIPE_WEBHOOK_SECRET"] = os.environ.get("STRIPE_WEBHOOK_SECRET", None)
 
-    # Handle the tips domain for profile verification
-    app.config["SERVER_NAME"] = os.getenv("SERVER_NAME")
-    app.config["PREFERRED_URL_SCHEME"] = "https" if os.getenv("SERVER_NAME") is not None else "http"
+    app.config.from_mapping(config)
+    configure_jinja(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-    # jinja configs
+    routes.init_app(app)
+    for module in [admin, settings]:
+        app.register_blueprint(module.create_blueprint())
+
+    if app.config.get("STRIPE_SECRET_KEY"):
+        app.register_blueprint(premium.create_blueprint(app))
+        # Initialize Stripe
+        with app.app_context():
+            premium.init_stripe()
+
+    @app.errorhandler(404)
+    def page_not_found(e: Exception) -> Response:
+        flash("⛓️‍💥 That page doesn't exist.", "warning")
+        return redirect(url_for("index"))
+
+    # Add Onion-Location header to all responses
+    if onion := app.config.get("ONION_HOSTNAME"):
+
+        @app.after_request
+        def add_onion_location_header(response: Response) -> Response:
+            response.headers["Onion-Location"] = f"http://{onion}{request.path}"
+            return response
+
+    # Register custom CLI commands
+    register_commands(app)
+
+    return app
+
+
+def configure_jinja(app: Flask) -> None:
     app.jinja_env.globals["hushline_version"] = __version__
     app.jinja_env.globals["directory_verified_tab_enabled"] = app.config[
         "DIRECTORY_VERIFIED_TAB_ENABLED"
     ]
-    if app.config.get("FLASK_ENV", None) == "development":
+    if app.config.get("FLASK_ENV") == "development":
         app.logger.info("Development environment detected, enabling jinja2.StrictUndefined")
         app.jinja_env.undefined = StrictUndefined
 
@@ -83,28 +71,8 @@ def create_app() -> Flask:
     app.jinja_env.globals["smtp_forwarding_message_html"] = app.config[
         "SMTP_FORWARDING_MESSAGE_HTML"
     ]
-    if onion_hostname := app.config.get("ONION_HOSTNAME", None):
+    if onion_hostname := app.config.get("ONION_HOSTNAME"):
         app.jinja_env.globals["onion_hostname"] = onion_hostname
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    # Initialize Stripe
-    if app.config["STRIPE_SECRET_KEY"]:
-        with app.app_context():
-            premium.init_stripe()
-
-    routes.init_app(app)
-    for module in [admin, settings]:
-        app.register_blueprint(module.create_blueprint())
-
-    if app.config["STRIPE_SECRET_KEY"]:
-        app.register_blueprint(premium.create_blueprint(app))
-
-    @app.errorhandler(404)
-    def page_not_found(e: Exception) -> Response:
-        flash("⛓️‍💥 That page doesn't exist.", "warning")
-        return redirect(url_for("index"))
 
     @app.context_processor
     def inject_variables() -> dict[str, Any]:
@@ -116,21 +84,6 @@ def create_app() -> Flask:
         if "user_id" in session:
             data["user"] = db.session.get(User, session["user_id"])
         return data
-
-    # Add Onion-Location header to all responses
-    if app.config["ONION_HOSTNAME"]:
-
-        @app.after_request
-        def add_onion_location_header(response: Response) -> Response:
-            response.headers["Onion-Location"] = (
-                f"http://{app.config['ONION_HOSTNAME']}{request.path}"
-            )
-            return response
-
-    # Register custom CLI commands
-    register_commands(app)
-
-    return app
 
 
 def register_commands(app: Flask) -> None:
---
+++ tests/test_config.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+import pytest
+
+from hushline.config import _JSON_CFG_PREFIX, _STRING_CFG_PREFIX, ConfigParseError, load_config
+
+CFG_NAME = "DOES_NOT_EXIST"
+
+
+def test_config_parse_string() -> None:
+    env = dict(**os.environ)
+    value = "test-123"
+    env[_STRING_CFG_PREFIX + CFG_NAME] = value
+
+    cfg = load_config(env)
+    assert cfg[CFG_NAME] == value
+
+
+def test_config_parse_json() -> None:
+    env = dict(**os.environ)
+    value = "test-123"
+    env[_JSON_CFG_PREFIX + CFG_NAME] = json.dumps(value)
+
+    cfg = load_config(env)
+    assert cfg[CFG_NAME] == value
+
+    value_int = 123
+    env[_JSON_CFG_PREFIX + CFG_NAME] = json.dumps(value_int)
+
+    cfg = load_config(env)
+    assert cfg[CFG_NAME] == value_int
+
+
+def test_config_parse_json_fail() -> None:
+    env = dict(**os.environ)
+    key = _JSON_CFG_PREFIX + CFG_NAME
+    value = "string not encoded as json"
+    env[key] = value
+
+    with pytest.raises(ConfigParseError) as e_info:
+        load_config(env)
+    assert key in str(e_info.value)
+    assert value not in str(e_info.value)
