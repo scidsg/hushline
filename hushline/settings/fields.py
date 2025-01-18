b080f5ba12416c3cdae0fa0df5eff95e6669dea2
Micah Lee <micah@micahflee.com>
2025-01-17 16:11:54 -0800

    Add support for creating fields, and displaying the fields

--- hushline/settings/fields.py
+++ hushline/settings/fields.py
@@ -2,6 +2,8 @@ from typing import Tuple
 
 from flask import (
     Blueprint,
+    current_app,
+    flash,
     render_template,
     request,
     session,
@@ -10,8 +12,9 @@ from werkzeug.wrappers.response import Response
 
 from hushline.auth import authentication_required
 from hushline.db import db
-from hushline.model import User
+from hushline.model import FieldDefinition, FieldType, User
 from hushline.settings.forms import FieldForm
+from hushline.utils import redirect_to_self
 
 
 def register_fields_routes(bp: Blueprint) -> None:
@@ -25,11 +28,26 @@ def register_fields_routes(bp: Blueprint) -> None:
             raise Exception("Username not found")
 
         if request.method == "POST":
-            field_form = FieldForm(request.form)
+            field_form = FieldForm()
             if field_form.validate():
+                current_app.logger.info(f"Field form validated: {field_form.data}")
+
                 # Create a new field
-                if "add_field" in request.form:
-                    pass
+                if field_form.submit.name in request.form:
+                    current_app.logger.info("Adding new field")
+                    field_definition = FieldDefinition(
+                        username,
+                        field_form.label.data,
+                        FieldType(field_form.field_type.data),
+                        field_form.required.data,
+                        field_form.enabled.data,
+                        field_form.encrypted.data,
+                        field_form.choices.data,
+                    )
+                    db.session.add(field_definition)
+                    db.session.commit()
+                    flash("New field added.")
+                    return redirect_to_self()
 
                 # Update an existing field
                 if "update_field" in request.form:
@@ -47,15 +65,18 @@ def register_fields_routes(bp: Blueprint) -> None:
                 if "move_down" in request.form:
                     pass
 
-        field_forms = [FieldForm(
-            id=field.id,
-            label=field.label,
-            field_type=field.field_type,
-            choices=field.choices,
-            encrypted=field.encrypted,
-            required=field.required,
-            enabled=field.enabled,
-        ) for field in username.message_fields]
+        field_forms = [
+            FieldForm(
+                id=field.id,
+                label=field.label,
+                field_type=field.field_type,
+                choices=field.choices,
+                encrypted=field.encrypted,
+                required=field.required,
+                enabled=field.enabled,
+            )
+            for field in username.message_fields
+        ]
 
         new_field_form = FieldForm()
 
