a273c8ac83fdb68d9bb9c042313ee2ff3330f3cd
Micah Lee <micah@micahflee.com>
2025-04-03 13:20:50 -0700

    Test that the alert is hidden if there are users

--- tests/test_first_user.py
+++ tests/test_first_user.py
@@ -22,3 +22,19 @@ def test_no_users_register_should_show_alert(client: FlaskClient) -> None:
     assert response.status_code == 200
 
     assert "Create the Admin User" in response.text
+
+
+def test_some_users_register_should_hide_alert(client: FlaskClient, user_password: str) -> None:
+    user_count = db.session.query(User).count()
+    assert user_count == 0
+
+    user = User(password=user_password)
+    user.tier_id = 1
+    db.session.add(user)
+    db.session.commit()
+    assert db.session.query(User).count() == 1
+
+    response = client.get(url_for("register"))
+    assert response.status_code == 200
+
+    assert "Create the Admin User" not in response.text
