7edcccd79f021682b195e749b86caaf9ec2652b9
Micah Lee <micah@micahflee.com>
2025-04-03 13:39:15 -0700

    Make the first user an admin, and show them in the directory

--- hushline/routes/auth.py
+++ hushline/routes/auth.py
@@ -51,6 +51,9 @@ def register_auth_routes(app: Flask) -> None:
             # Use the existing math problem from the session
             math_problem = session.get("math_problem", "Error: CAPTCHA not generated.")
 
+        # Check if this is the first user
+        first_user = db.session.query(User).count() == 0
+
         if form.validate_on_submit():
             captcha_answer = request.form.get("captcha_answer", "")
             app.logger.debug(f"Session math_answer: {session.get('math_answer')}")
@@ -97,10 +100,20 @@ def register_auth_routes(app: Flask) -> None:
                 )
 
             user = User(password=password)
+
+            # If this is the first user, set them as admin
+            if first_user:
+                user.is_admin = True
+
             db.session.add(user)
             db.session.flush()
 
             username = Username(_username=username, user_id=user.id, is_primary=True)
+
+            # If this is the first user, show them in the directory
+            if first_user:
+                username.show_in_directory = True
+
             db.session.add(username)
             db.session.commit()
 
@@ -108,9 +121,6 @@ def register_auth_routes(app: Flask) -> None:
 
             flash("Registration successful!", "success")
             return redirect(url_for("login"))
-
-        # Check if this is the first user
-        first_user = db.session.query(User).count() == 0
 
         return render_template(
             "register.html",
--- tests/test_first_user.py
+++ tests/test_first_user.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import url_for
 from flask.testing import FlaskClient
+from helpers import get_captcha_from_session_register
 
 from hushline.db import db
-from hushline.model import User
+from hushline.model import User, Username
 
 
 def test_no_users_redirect_to_register(client: FlaskClient) -> None:
@@ -38,3 +41,62 @@ def test_some_users_register_should_hide_alert(client: FlaskClient, user_passwor
     assert response.status_code == 200
 
     assert "Create the Admin User" not in response.text
+
+def test_first_user_is_admin(client: FlaskClient) -> None:
+    user_count = db.session.query(User).count()
+    assert user_count == 0
+
+    os.environ["REGISTRATION_CODES_REQUIRED"] = "False"
+    username = "test_user"
+
+    captcha_answer = get_captcha_from_session_register(client)
+
+    response = client.post(
+        url_for("register"),
+        data={
+            "username": username,
+            "password": "SecurePassword123!",
+            "captcha_answer": captcha_answer,
+        },
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
+    assert "Registration successful!" in response.text
+
+    # The user should be an admin
+    user = db.session.scalars(db.select(User)).one()
+    assert user.is_admin
+    assert user.primary_username.show_in_directory
+
+def test_second_user_is_not_admin(client: FlaskClient, user_password: str) -> None:
+    user_count = db.session.query(User).count()
+    assert user_count == 0
+
+    user = User(password=user_password)
+    user.tier_id = 1
+    user.is_admin = True
+    db.session.add(user)
+    db.session.commit()
+    assert db.session.query(User).count() == 1
+
+    os.environ["REGISTRATION_CODES_REQUIRED"] = "False"
+    username = "test_user"
+
+    captcha_answer = get_captcha_from_session_register(client)
+
+    response = client.post(
+        url_for("register"),
+        data={
+            "username": username,
+            "password": "SecurePassword123!",
+            "captcha_answer": captcha_answer,
+        },
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
+    assert "Registration successful!" in response.text
+
+    # The user should not be an admin
+    uname = db.session.scalars(db.select(Username).filter_by(_username=username)).one()
+    assert not uname.user.is_admin
+    assert not uname.show_in_directory
