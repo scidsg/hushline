2c0dd743a2f36fe2466c7df9d41728c0efe2677d
Micah Lee <micah@micahflee.com>
2025-04-22 10:19:43 -0700

    Fix TOCTOU race conditions

--- hushline/settings/delete_account.py
+++ hushline/settings/delete_account.py
@@ -30,51 +30,55 @@ def register_delete_account_routes(bp: Blueprint) -> None:
             flash("Please log in to continue.")
             return redirect(url_for("login"))
 
-        user = db.session.get(User, user_id)
-        if user:
-            if user.is_admin:
-                admin_count = db.session.query(User).filter_by(is_admin=True).count()
-                if admin_count == 1:
-                    flash("⛔️ You cannot delete the only admin account")
-                    return abort(400)
+        with db.session.begin_nested():
+            user = db.session.get(User, user_id)
+            if user:
+                if user.is_admin:
+                    admin_count = db.session.query(User).filter_by(is_admin=True).count()
+                    if admin_count == 1:
+                        flash("⛔️ You cannot delete the only admin account")
+                        return abort(400)
 
-            # Delete field values and definitions
-            usernames = db.session.scalars(db.select(Username).filter_by(user_id=user.id)).all()
-            username_ids = [username.id for username in usernames]
+                # Delete field values and definitions
+                usernames = db.session.scalars(db.select(Username).filter_by(user_id=user.id)).all()
+                username_ids = [username.id for username in usernames]
 
-            # Delete all FieldValue entries related to the user's usernames
-            db.session.execute(
-                db.delete(FieldValue).where(
-                    FieldValue.field_definition_id.in_(
-                        db.select(FieldDefinition.id).where(
-                            FieldDefinition.username_id.in_(username_ids)
+                # Delete all FieldValue entries related to the user's usernames
+                db.session.execute(
+                    db.delete(FieldValue).where(
+                        FieldValue.field_definition_id.in_(
+                            db.select(FieldDefinition.id).where(
+                                FieldDefinition.username_id.in_(username_ids)
+                            )
                         )
                     )
                 )
-            )
 
-            # Delete all FieldDefinition entries related to the user's usernames
-            db.session.execute(
-                db.delete(FieldDefinition).where(FieldDefinition.username_id.in_(username_ids))
-            )
-
-            # Delete messages and related data
-            db.session.execute(
-                db.delete(Message).filter(
-                    Message.username_id.in_(db.select(Username.id).filter_by(user_id=user.id))
+                # Delete all FieldDefinition entries related to the user's usernames
+                db.session.execute(
+                    db.delete(FieldDefinition).where(FieldDefinition.username_id.in_(username_ids))
                 )
-            )
-            db.session.execute(db.delete(MessageStatusText).filter_by(user_id=user.id))
-            db.session.execute(db.delete(AuthenticationLog).filter_by(user_id=user.id))
 
-            # Delete username and finally the user
-            db.session.execute(db.delete(Username).filter_by(user_id=user.id))
-            db.session.delete(user)
-            db.session.commit()
+                # Delete messages and related data
+                db.session.execute(
+                    db.delete(Message).filter(
+                        Message.username_id.in_(db.select(Username.id).filter_by(user_id=user.id))
+                    )
+                )
+                db.session.execute(db.delete(MessageStatusText).filter_by(user_id=user.id))
+                db.session.execute(db.delete(AuthenticationLog).filter_by(user_id=user.id))
 
-            session.clear()
-            flash("🔥 Your account and all related information have been deleted.")
-            return redirect(url_for("index"))
+                # Delete username and finally the user
+                db.session.execute(db.delete(Username).filter_by(user_id=user.id))
+                db.session.delete(user)
+                db.session.commit()
+
+                session.clear()
+                flash("🔥 Your account and all related information have been deleted.")
+                return redirect(url_for("index"))
+
+        # Commit the transaction
+        db.session.commit()
 
         flash("User not found. Please log in again.")
         return redirect(url_for("login"))
