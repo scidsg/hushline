901d115621c26388fb34ed258bd6339931163647
Glenn <git@scidsg.org>
2025-12-10 16:07:33 -0800

    Update images.js

--- assets/js/images.js
+++ assets/js/images.js
@@ -5,6 +5,7 @@ import "./../img/dropdown.png";
 import "./../img/empty.png";
 import "./../img/icon-chevron-dm.png";
 import "./../img/icon-chevron.png";
+import "./../img/icon-clear.png";
 import "./../img/icon-menu-light.png";
 import "./../img/icon-menu.png";
 import "./../img/icon-search.png";
