b50b057bf5c828581034b8b12899e8dcabf44934
Glenn <git@scidsg.org>
2025-12-19 14:29:14 -0800

    update url encoding for profile urls

--- assets/js/directory.js
+++ assets/js/directory.js
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     <div class="badgeContainer">${badgeContainer}</div>
                     ${bioHighlighted ? `<p class="bio">${bioHighlighted}</p>` : ""}
                     <div class="user-actions">
-                        <a href="${pathPrefix}/to/${user.primary_username}">View Profile</a>
+                        <a href="${pathPrefix}/to/${encodeURIComponent(user.primary_username)}">View Profile</a>
                         ${isSessionUser ? `<a href="#" class="report-link" data-username="${user.primary_username}" data-display-name="${user.display_name || user.primary_username}" data-bio="${user.bio ?? "No bio"}">Report Account</a>` : ``}
                     </div>
                 `;
