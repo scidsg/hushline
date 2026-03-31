25d1ed890ed369802a765e539689402d2d94284b
hushline-dev <glenn.sorrentino@proton.me>
2026-03-31 16:16:29 -0700

    Update directory_verified.js

--- hushline/static/js/directory_verified.js
+++ hushline/static/js/directory_verified.js
@@ -1091,7 +1091,9 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       controller.metadataSearch = search;
-      const metadataRequest = fetch(`${directoryPath}/${controller.metadataPath}${search}`)
+      const metadataRequest = fetch(
+        `${directoryPath}/${controller.metadataPath}${search}`,
+      )
         .then((response) => {
           if (!response.ok) {
             throw new Error("Network response was not ok");
