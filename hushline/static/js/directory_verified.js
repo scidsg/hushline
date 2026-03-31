13fa298a3029b3630ba08fd4408bf89f4b00aeb1
hushline-dev <glenn.sorrentino@proton.me>
2026-03-31 16:06:58 -0700

    Fix CI template database setup

--- hushline/static/js/directory_verified.js
+++ hushline/static/js/directory_verified.js
@@ -1091,9 +1091,7 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       controller.metadataSearch = search;
-      const metadataRequest = fetch(
-        `${directoryPath}/${controller.metadataPath}${search}`,
-      )
+      const metadataRequest = fetch(`${directoryPath}/${controller.metadataPath}${search}`)
         .then((response) => {
           if (!response.ok) {
             throw new Error("Network response was not ok");
